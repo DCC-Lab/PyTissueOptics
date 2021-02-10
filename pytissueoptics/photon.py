FIxed refraction code, because of a sign, but needs cleanup
It feels like either 1) an IntersectElement class should be created to avoid  calculation many parameter several times or 2) functions for refraction should be in Vector.

pytissueoptics/photon.py
@@ -104,9 +104,6 @@ class Photon:
         know there is no refraction, and we simply return.
         """
 
-        if surface.indexInside == surface.indexOutside:
-            return
-
         normal = None
 
         if self.ez.dot(surface.normal) > 0:
@@ -122,13 +119,16 @@ class Photon:
 
         thetaIn, planeOfIncidenceNormal, actualNormal = self.ez.angleOfIncidence(normal)
 
-        if thetaIn == 0:
-            # Normal incidence
-            return
 
-        thetaOut = math.asin(n1*math.sin(thetaIn)/n2)
+        sinThetaOut = n1*math.sin(thetaIn)/n2
+        if sinThetaOut > 1:
+            # We should not be here.
+            raise ValueError("Can't refract beyond angle of total reflection")
 
-        self.ez.rotateAround(planeOfIncidenceNormal, thetaOut-thetaIn)
+        thetaOut = np.arcsin(sinThetaOut)
+        # TODO: why not thetaOut - thetaIn? Depends on planeOfIncidence but
+        # I expected thetaOut-thetaIn
+        self.ez.rotateAround(planeOfIncidenceNormal, thetaIn-thetaOut)
 
     def roulette(self):
         chance = 0.1

pytissueoptics/tests/testsPhotons.py
@@ -41,21 +41,74 @@ class TestPhoton(envtest.PyTissueTestCase):
         p.ez.rotateAround(planeOfIncidenceNormal, 2*thetaIn-np.pi)
         self.assertAlmostEqual((p.ez - Vector(0,-1,-1).normalized()).norm(), 0, 6)
 
-    def testReflect(self):
+    def testReflectNormalIncidence(self):
         s = Surface(origin=oHat, a=xHat, b=yHat, normal=zHat)
         p = Photon(position=Vector(0,0,0), direction=zHat)
         p.reflect(s)
         self.assertTrue( (p.ez+zHat).norm() < 1e-6)
 
-    def testRefractInto(self):
+    def testNoRefract(self):
+        s = Surface(origin=oHat, a=xHat, b=yHat, normal=-zHat)
+        s.indexInside = 1.0
+        s.indexOutside = 1.0
+
+        p = Photon(position=Vector(0,0,0), direction=zHat)
+        p.refract(s)
+        self.assertTrue( p.ez.isAlmostEqualTo(zHat))
+
+    def testNormalRefract(self):
         s = Surface(origin=oHat, a=xHat, b=yHat, normal=-zHat)
         s.indexInside = 1.4
         s.indexOutside = 1.0
 
         p = Photon(position=Vector(0,0,0), direction=zHat)
         p.refract(s)
-        self.assertTrue( (p.ez-zHat).norm() < 1e-6)
+        self.assertTrue( p.ez.isAlmostEqualTo(zHat))
 
+    def testRefractIntoFrom(self):
+        s = Surface(origin=oHat, a=xHat, b=yHat, normal=zHat)
+        s.indexInside = 1.0
+        s.indexOutside = 1.4
+
+        for i in range(100):
+
+            thetaIn = (i-50)*np.pi/4/50
+            sinIn = np.sin(thetaIn)
+            cosIn = np.cos(thetaIn)
+            vIn = UnitVector(0,sinIn,cosIn)
+            sinOut = np.sin(thetaIn)*s.indexInside/s.indexOutside
+            cosOut = sqrt(1-sinOut*sinOut)
+            vOut = UnitVector(0,sinOut, cosOut)
+
+            p = Photon(position=Vector(0,0,0), direction=vIn)
+            
+            p.refract(s)
+
+            self.assertTrue( p.ez.isAlmostEqualTo(vOut), "ez: {0} vOut: {1} vIn {2}".format(p.ez, vOut, vIn))
+
+    def testRefractOutOf(self):
+        s = Surface(origin=oHat, a=xHat, b=yHat, normal=zHat)
+        s.indexInside = 1.2
+        s.indexOutside = 1.0
+
+        for i in range(100):
+
+            thetaIn = (i-50)*np.pi/2.1/50
+            sinIn = np.sin(thetaIn)
+            cosIn = np.cos(thetaIn)
+            vIn = UnitVector(0,sinIn,cosIn)
+            sinOut = np.sin(thetaIn)*s.indexInside/s.indexOutside
+            if abs(sinOut) >= 1:
+                continue
+            thetaOut = np.arcsin(sinOut)
+            cosOut = np.cos(thetaOut)
+
+            vOut = UnitVector(0,sinOut, cosOut)
+            p = Photon(position=Vector(0,0,0), direction=vIn)
+
+            p.refract(s)
+
+            self.assertTrue( p.ez.isAlmostEqualTo(vOut), "ez: {0} vOut: {1} vIn {2}".format(p.ez, vOut, vIn))
 
 if __name__ == '__main__':
     envtest.main()