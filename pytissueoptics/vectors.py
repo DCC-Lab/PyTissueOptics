ad2bd2af918e57889eae15ad546de90c5a42e474
Daniel <dccote@cervo.ulaval.ca>
2021-03-03 17:34:56 -0500

    Added Scalars class, not tested.

--- pytissueoptics/vectors.py
+++ pytissueoptics/vectors.py
@@ -264,4 +264,57 @@ class NumpyVectors:
     def randomUnitary(cls, N):
         pass
 
+class Scalars:
+    """ An array of scalars that is compatible with operations on Vectors 
+    There is a reason for not using numpy.array directly: we want to
+    add new functions that will be specific to our problem here,
+    and Python does not allow us to extend system classes.
+    """
+    def __init__(self, array, N):
+        self.v = np.array(array)
+        self._iteration = 0
+
+    def __iter__(self):
+        self._iteration = 0
+        return self
+
+    def __next__(self):
+        if self._iteration < self.count:
+            result = self.v[self._iteration]
+            self._iteration += 1
+            return result
+        else:
+            raise StopIteration
+
+    def __len__(self):
+        return len(self.v)
+
+    def __mul__(self, scale):
+        return Scalars(self.v*scale)
+
+    def __rmul__(self, scale):
+        return Scalars(self.v*scale)
+
+    def __truediv__(self, scale):
+        return Scalars(self.v/scale)
+
+    def __add__(self, rhs):
+        return Scalars([v1+v2 for (v1,v2) in list(zip(self.v, rhs.v))])
+
+    def __neg__(self):
+        return Scalars([-v1 for v1 in self.v])
+
+    def __sub__(self, rhs):
+        return Scalars([v1-v2 for (v1,v2) in list(zip(self.v, rhs.v))])
+
+    def __getitem__(self, index):
+        return self.v[index]
+
+    def __setitem__(self, index, newvalue): 
+        self.v[index] = newvalue
+
+    def __eq__(self, rhs):
+        each = [v1 == v2 for (v1,v2) in list(zip(self.v, rhs.v))]
+        return np.array(each).all()
+
 Vectors = NativeVectors
