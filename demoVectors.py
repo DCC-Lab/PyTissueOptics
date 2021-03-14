7f905ce2655ad16eb8ebaefc43ffb153e78e3834
Marc-André <marc-andre.vigneault.02@hotmail.com>
2021-03-14 10:31:27 -0400

    demoVectors profiling #1

--- demoVectors.py
+++ demoVectors.py
@@ -2,6 +2,7 @@ import numpy as np
 import cupy as cp
 import random
 from pytissueoptics import *
+from cupyx import optimizing
 from time import time_ns
 
 
@@ -19,8 +20,8 @@ def getScatteringAngle(N):
         phi.append(random.random() * 2 * np.pi)
     return theta, phi
 
-batches = 10
-N = 5000000
+batches = 2
+N = 8000000
 
 position = Vectors(N=N)
 direction = Vectors([[0, 0, 1]]*N)
@@ -30,21 +31,31 @@ er = Vectors([[1, 0, 0]]*N)
 weight = Scalars([1.0]*N)
 
 isAlive = True
+
+# cuda stuff
 print(cp.cuda.runtime.getDeviceCount())
+mempool = cp.get_default_memory_pool()
+pinned_mempool = cp.get_default_pinned_memory_pool()
 
 timea = time_ns()
-for _ in range(batches):
-    while isAlive:
-        theta = Scalars.random(N=N) * 2 * np.pi
-        phi = Scalars.random(N=N) * 2 * np.pi
+for i in range(batches):
+    count = 0
+    with optimizing.optimize():
+        while isAlive:
+            count += 1
+            theta = Scalars.random(N=N) * 2 * np.pi
+            phi = Scalars.random(N=N) * 2 * np.pi
 
-        d = Scalars.random(N=N)
+            d = Scalars.random(N=N)
 
-        er.rotateAround(direction, phi)
-        direction.rotateAround(er, theta)
-        position = position + direction*d
-        weight *= 0.9
-        isAlive = (weight.v > 0.001).any()
+            er.rotateAround(direction, phi)
+            direction.rotateAround(er, theta)
+            position = position + direction*d
+            weight *= 0.9
+            isAlive = (weight.v > 0.001).any()
+
+        if not count % 10:
+            print(f"Pool Available:{mempool.total_bytes()/1000000}MB, Allocated:{mempool.used_bytes()/1000000}MB, Pool Blocks:{pinned_mempool.n_free_blocks()}")  # 512
 
     weight = Scalars([1.0] * N)
     isAlive = True
