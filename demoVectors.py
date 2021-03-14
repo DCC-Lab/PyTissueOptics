f109e62a500a8d464eb56f44e3ee0a31a857de62
Marc-André <marc-andre.vigneault.02@hotmail.com>
2021-03-13 22:47:44 -0400

    Working CupyVersion of Vectors. Had to implement CupyScalars.

--- demoVectors.py
+++ demoVectors.py
@@ -1,6 +1,8 @@
 import numpy as np
+import cupy as cp
 import random
 from pytissueoptics import *
+from time import time_ns
 
 
 def getScatteringDistance(N, mu_s):
@@ -24,17 +26,23 @@ N = 100000
 position = Vectors(N=N)
 direction = Vectors([[0, 0, 1]]*N)
 er = Vectors([[1, 0, 0]]*N)
-weight = NumpyScalars(np.array([1.0]*N))
+weight = Scalars(np.array([1.0]*N))
 
 isAlive = True
+
+timea = time_ns()
 while isAlive:
     theta = NumpyScalars.random(N=N) * 2 * np.pi
     phi = NumpyScalars.random(N=N) * 2 * np.pi
 
-    d = NumpyScalars(getScatteringDistance(N, mu_s=30))
+    d = Scalars(getScatteringDistance(N, mu_s=30))
 
     er.rotateAround(direction, phi)
     direction.rotateAround(er, theta)
     position = position + direction*d
     weight *= 0.9
     isAlive = (weight.v > 0.001).any()
+
+timeb = time_ns()
+
+print(f"{N} photons in {(timeb-timea)/1000000} ms.")
