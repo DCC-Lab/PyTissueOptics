2e1920a970d62c1e2aeca25749ba88969c300325
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2021-04-21 17:31:30 -0400

    commented surface assertion + changed matplotlib backend to tkagg

--- pytissueoptics/__init__.py
+++ pytissueoptics/__init__.py
@@ -2,7 +2,7 @@ import matplotlib
 import os
 # must be before importing matplotlib.pyplot or pylab!
 if os.name == 'posix' and "DISPLAY" not in os.environ:
-    matplotlib.use('Agg')
+    matplotlib.use('tkagg')
 
 import math
 
