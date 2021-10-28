44a11f61e20000627d600452a00b4110d0365d61
Marc-Andre Vigneault <marc-andre.vigneault.02@hotmail.com>
2021-10-28 16:21:10 -0400

    coded basic of the Photons class.

--- pytissueoptics/photon.py
+++ pytissueoptics/photon.py
@@ -101,33 +101,68 @@ class Photon:
 
 
 class Photons:
-    def __init__(self, position=None, direction=None):
-        n = len(position)
-        self.r = Vectors(position)
-        self.ez = Vectors(direction)  # Propagation direction vector
-        self.er = Vectors( [Vector(0, 1, 0)]*n )
-
-        self.er.select(logicalNot(self.er.isPerpendicularTo(self.ez)))
-        self.er.replaceSelected(self.ez.anyPerpendicular())
-        self.er.selectAll()
-
-        # We don't need to keep el, because it is obtainable from ez and er
-        self.weight = Scalars([1.0]*n)
+    def __init__(self, positions=Vectors(N=1000), directions=Vectors([zHat] * 1000)):
+        self.N = len(positions)
+        self.r = positions
+        self.ez = directions
+        self.er = self.ez.anyPerpendicular()
+        self.wavelength = None
+        self.weight = Scalars([1] * self.N)
         self.path = None
+        self.origin = Vectors(N=self.N)
+        self._iteration = 0
+
+    def __len__(self):
+        return self.N
+
+    def __getitem__(self, index):
+        return Photon(position=self.r[index], direction=self.ez[index], weight=self.weight[index])
+
+    def __setitem__(self, index, photon):
+        self.r[index] = photon.r
+        self.z[index] = photon.z
+        self.ez[index] = photon.ez
+        self.er[index] = photon.er
+        self.weight[index] = photon.weight
+        self.origin[index] = photon.origin
+
+    def __iter__(self):
+        self._iteration = 0
+        return self
+
+    def __next__(self):
+        if self._iteration < self.N:
+            result = self[self._iteration]
+            self._iteration += 1
+            return result
+        else:
+            raise StopIteration
 
     @property
-    def el(self) -> Vectors:
+    def localPosition(self):
+        return self.r
+
+    @property
+    def globalPosition(self):
+        return self.r + self.origin
+
+    @property
+    def el(self):
         return self.ez.cross(self.er)
 
     @property
-    def isAlive(self) -> bool:
-        return (self.weight.v > 0).all()
+    def areAllDead(self):
+        return all(self.weight == 0)
+
+    @property
+    def isAlive(self):
+        return self.weight > 0
 
     def transformToLocalCoordinates(self, origin):
-        self.r = self.r - [origin]* len(self.r)
+        self.r = self.r - origin
 
     def transformFromLocalCoordinates(self, origin):
-        self.r = self.r + [origin]* len(self.r)
+        self.r = self.r + origin
 
     def moveBy(self, d):
         self.r.addScaled(self.ez, d)
@@ -136,19 +171,25 @@ class Photons:
         self.er.rotateAround(self.ez, phi)
         self.ez.rotateAround(self.er, theta)
 
-    def decreaseWeightBy(self, delta):
-        self.weight -= delta
-        if self.weight < 0:
-            self.weight = 0
+    def decreaseWeightBy(self, deltas):
+        self.weight -= deltas
+        self.weight.conditional_lt(0, 0, self.weight.v)
 
     def roulette(self):
-        n = len(self.position)
-
         chance = 0.1
+        rouletteMask = self.weight <= 1e-4
+        photonsKillMask = (Scalars.random(self.N)) > chance
+        photonsKillMask = rouletteMask.logical_and(photonsKillMask)
+        self.removePhotonsWeights(photonsKillMask)
 
-        rouletteMask = (self.weight.v < 1e-4)
+    def removePhotonsWeights(self, killMask):
+        self.weight = self.weight * ~killMask
 
-        live = (Scalars.random(n) > chance)
-        die = logicalNot(live)
-        factor = dontTouchMask + rouletteMask * live/chance
+    def deflect(self):
+        pass
 
+    def reflect(self):
+        pass
+
+    def refract(self):
+        pass
