b662e67488157c9bd3b76e643a02b6f74f29e45d
JLBegin <ludovick.begin@gmail.com>
2021-12-06 12:54:28 -0500

    testsGeometryIT: start creating test harness

---
+++ pytissueoptics/testsIT/testsGeometryIT.py
@@ -0,0 +1,39 @@
+import unittest
+
+from pytissueoptics import Geometry, Photon, Material
+
+
+class TestGeometryIT(unittest.TestCase):
+
+    def testCreateGeometry(self):
+        geometry = Geometry()
+        self.assertIsNotNone(geometry)
+
+    def testGivenNoMaterial_whenPropagatePhoton_shouldRaiseError(self):
+        geometry = Geometry()
+        photon = Photon()
+
+        with self.assertRaises(AttributeError):
+            geometry.propagate(photon)
+
+    def testCreateMaterial(self):
+        material = Material()
+        self.assertIsNotNone(material)
+
+    @unittest.skip("Default material will propagate the photon indefinitely.")
+    def testGivenDefaultMaterial_whenPropagatePhoton_shouldPropagateIndefinitely(self):
+        material = Material()
+        geometry = Geometry(material=material)
+        photon = Photon()
+
+        geometry.propagate(photon)
+
+    def testWhenPropagatePhoton_shouldPropagateThePhotonUntilDead(self):
+        aMaterial = Material(mu_s=2, mu_a=2, g=0.8, index=1.2)
+        geometry = Geometry(material=aMaterial)
+        photon = Photon()
+
+        geometry.propagate(photon)
+
+        self.assertFalse(photon.isAlive)
+
