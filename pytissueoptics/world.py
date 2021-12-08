a1602c304e13980f09a3bf82d3a50018230148d2
JLBegin <ludovick.begin@gmail.com>
2021-12-08 15:17:49 -0500

    World: clean up contract by tagging private methods with underscore

--- pytissueoptics/world.py
+++ pytissueoptics/world.py
@@ -1,5 +1,5 @@
-import signal
 from .detector import *
+import signal
 
 
 class World:
@@ -9,29 +9,31 @@ class World:
         self.verbose = False
         self.countNotSupposedToBeThere = 0
 
-    def totalSourcePhotons(self) -> float:
-        total = 0
-        for source in self.sources:
-            total += source.maxCount
-        return total
+    def place(self, anObject, position):
+        if isinstance(anObject, Geometry) or isinstance(anObject, Detector):
+            anObject.origin = position
+            self.geometries.add(anObject)
+        elif isinstance(anObject, Source):
+            anObject.origin = position
+            self.sources.add(anObject)
 
     def compute(self, graphs, progress=False):
-        self.startCalculation()
+        self._startCalculation()
         N = 0
         for source in self.sources:
             N += source.maxCount
 
             for i, photon in enumerate(source):
-                currentGeometry = self.contains(photon.globalPosition)
+                currentGeometry = self._contains(photon.globalPosition)
                 while photon.isAlive:
                     if currentGeometry is not None:
                         # We are in an object, propagate in it
                         currentGeometry.propagate(photon)
                         # Then check if we are in another adjacent object
-                        currentGeometry = self.contains(photon.globalPosition)
+                        currentGeometry = self._contains(photon.globalPosition)
                     else:
                         # We are in free space (World). Find next object
-                        intersection = self.nextObstacle(photon)
+                        intersection = self._nextObstacle(photon)
                         if intersection is not None:
                             # We are hitting something, moving to surface
                             photon.moveBy(intersection.distance)
@@ -51,28 +53,30 @@ class World:
                         else:
                             photon.weight = 0
             if progress:
-                self.showProgress(i + 1, maxCount=source.maxCount, graphs=graphs)
+                self._showProgress(i + 1, maxCount=source.maxCount, graphs=graphs)
 
-        duration = self.completeCalculation()
+        duration = self._completeCalculation()
         if progress:
             print("{0:.1f} ms per photon\n".format(duration * 1000 / N))
 
-    def place(self, anObject, position):
-        if isinstance(anObject, Geometry) or isinstance(anObject, Detector):
-            anObject.origin = position
-            self.geometries.add(anObject)
-        elif isinstance(anObject, Source):
-            anObject.origin = position
-            self.sources.add(anObject)
+    def report(self, graphs=True):
+        for geometry in self.geometries:
+            geometry.report(totalSourcePhotons=self._totalSourcePhotons(), graphs=graphs)
 
-    def contains(self, worldCoordinates):
+    def _totalSourcePhotons(self) -> float:
+        total = 0
+        for source in self.sources:
+            total += source.maxCount
+        return total
+
+    def _contains(self, worldCoordinates):
         for geometry in self.geometries:
             localCoordinates = worldCoordinates - geometry.origin
             if geometry.contains(localCoordinates):
                 return geometry
-        return self
+        return None
 
-    def nextObstacle(self, photon):
+    def _nextObstacle(self, photon):
         if not photon.isAlive:
             return None
         distance = 1e7
@@ -90,28 +94,13 @@ class World:
 
         return closestIntersect
 
-    def allNextObstacles(self, photons):
-        impededPhotons = []
-        unimpededPhotons = []
-        intersects = []
-
-        for photon in photons:
-            intersect = self.nextObstacle(photon)
-            if intersect is not None:
-                intersects.append(intersect)
-                impededPhotons.append(photon)
-            else:
-                unimpededPhotons.append(photon)
-
-        return Photons(unimpededPhotons), (Photons(impededPhotons), FresnelIntersects(intersects))
-
-    def startCalculation(self):
+    def _startCalculation(self):
         if 'SIGUSR1' in dir(signal) and 'SIGUSR2' in dir(signal):
             # Trick to send a signal to code as it is running on Unix and derivatives
             # In the shell, use `kill -USR1 processID` to get more feedback
             # use `kill -USR2 processID` to force a save
-            signal.signal(signal.SIGUSR1, self.processSignal)
-            signal.signal(signal.SIGUSR2, self.processSignal)
+            signal.signal(signal.SIGUSR1, self._processSignal)
+            signal.signal(signal.SIGUSR2, self._processSignal)
 
         if len(self.geometries) == 0:
             raise SyntaxError("No geometries: you must create objects")
@@ -130,7 +119,7 @@ class World:
 
         self.startTime = time.time()
 
-    def completeCalculation(self) -> float:
+    def _completeCalculation(self) -> float:
         if 'SIGUSR1' in dir(signal) and 'SIGUSR2' in dir(signal):
             signal.signal(signal.SIGUSR1, signal.SIG_DFL)
             signal.signal(signal.SIGUSR2, signal.SIG_DFL)
@@ -139,14 +128,14 @@ class World:
         self.startTime = None
         return elapsed
 
-    def processSignal(self, signum, frame):
+    def _processSignal(self, signum, frame):
         if signum == signal.SIGUSR1:
             self.verbose = not self.verbose
             print('Toggling verbose to {0}'.format(self.verbose))
         elif signum == signal.SIGUSR2:
             print("Requesting save (not implemented)")
 
-    def showProgress(self, i, maxCount, graphs=False):
+    def _showProgress(self, i, maxCount, graphs=False):
         steps = 100
 
         if not self.verbose:
@@ -160,7 +149,3 @@ class World:
                 for geometry in self.geometries:
                     if geometry.stats is not None:
                         geometry.stats.showEnergy2D(plane='xz', integratedAlong='y', title="{0} photons".format(i))
-
-    def report(self, graphs=True):
-        for geometry in self.geometries:
-            geometry.report(totalSourcePhotons=self.totalSourcePhotons(), graphs=graphs)
