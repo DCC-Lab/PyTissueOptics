dcaeadc774ce1397a3afabcc464ff1f35e9961d2
JLBegin <ludovick.begin@gmail.com>
2022-02-03 14:50:10 -0500

    Vector: add true division

--- pytissueoptics/scene/geometry/vector.py
+++ pytissueoptics/scene/geometry/vector.py
@@ -37,6 +37,9 @@ class Vector:
     def __add__(self, other: 'Vector') -> 'Vector':
         return Vector(self._x + other.x, self._y + other.y, self._z + other.z)
 
+    def __truediv__(self, scalar: float) -> 'Vector':
+        return Vector(self._x / scalar, self._y / scalar, self._z / scalar)
+
     def add(self, other: 'Vector'):
         self._x += other.x
         self._y += other.y
