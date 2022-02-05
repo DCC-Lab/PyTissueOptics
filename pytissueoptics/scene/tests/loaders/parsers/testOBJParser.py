4ee4ebaacb72eef243e2e58c32f59d97a2e2c121
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-04 22:04:33 -0500

    correct test objparser

--- pytissueoptics/scene/tests/loaders/parsers/testOBJParser.py
+++ pytissueoptics/scene/tests/loaders/parsers/testOBJParser.py
@@ -7,13 +7,17 @@ class TestOBJParser(unittest.TestCase):
         with self.assertRaises(Exception):
             parser = OBJParser()
 
-
     def testWhenWrongExtension_shouldRaiseError(self):
         with self.assertRaises(TypeError):
-            parser = OBJParser("nightstand.dae")
+            parser = OBJParser("test.wrongExtension")
 
     def testWhenCorrectExtension_shouldNotDoAnything(self):
         parser = OBJParser("testCubeQuads.obj")
 
-    def testWhenCubeQuads_shouldGiveCorrectFacesNormalsAndGroups(self):
+    def testWhenTestCubeQuads_shouldGiveCorrectAmountOfVertices(self):
         parser = OBJParser("testCubeQuads.obj")
+        self.assertEqual(8, len(parser.vertices))
+
+    def testWhenTestCubeQuads_shouldGiveCorrectAmountOfNormals(self):
+        parser = OBJParser("testCubeQuads.obj")
+        self.assertEqual(6, len(parser.normals))
