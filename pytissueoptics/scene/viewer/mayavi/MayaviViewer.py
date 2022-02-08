7f76b5c5b83e893c0135a7f3b5881dd2891e793b
JLBegin <ludovick.begin@gmail.com>
2022-02-07 21:10:02 -0500

    MyaviViewer: checkstyle
    Imports order: std, 3rd-party, local.
    Max 120 chars per line.
    camelCase.

--- pytissueoptics/scene/viewer/mayavi/MayaviViewer.py
+++ pytissueoptics/scene/viewer/mayavi/MayaviViewer.py
@@ -1,8 +1,10 @@
+from typing import Union
+
 try:
     from mayavi import mlab
 except ImportError:
     pass
-from typing import Union
+
 from pytissueoptics.scene.viewer.mayavi import MayaviSolid
 from pytissueoptics.scene.solids import Sphere, Cuboid, Solid
 from pytissueoptics.scene.geometry import Vector, primitives
@@ -11,14 +13,16 @@ from pytissueoptics.scene.scene import Scene
 
 class MayaviViewer:
     def __init__(self):
-        self._scenes = {"DefaultScene": {"figure": mlab.figure(1, bgcolor=(0.11, 0.11, 0.11), fgcolor=(0.9, 0.9, 0.9)), "Solids": [], }}
+        self._scenes = {"DefaultScene": {"figure": mlab.figure(1, bgcolor=(0.11, 0.11, 0.11), fgcolor=(0.9, 0.9, 0.9)),
+                                         "Solids": [], }}
         self._view = {"azimuth": 0, "zenith": 0, "distance": None, "pointingTowards": None, "roll": None}
 
-    def add(self, other: 'Solid', representation="wireframe", line_width=0.25):
+    def add(self, other: 'Solid', representation="wireframe", lineWidth=0.25):
         assert other.primitive == primitives.TRIANGLE, "MavaviViewer currently only supports triangle mesh. "
         other = MayaviSolid(other)
         self._scenes["DefaultScene"]["Solids"].append(other)
-        mlab.triangular_mesh(*other.mesh.components, representation=representation, line_width=line_width, colormap="viridis")
+        mlab.triangular_mesh(*other.mesh.components, representation=representation, line_width=lineWidth,
+                             colormap="viridis")
 
     def _assignViewPoint(self):
         azimuth, elevation, distance, towards, roll = (self._view[key] for key in self._view)
