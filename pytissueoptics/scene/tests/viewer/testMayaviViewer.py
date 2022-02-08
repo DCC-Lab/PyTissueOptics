6c45897714d248bcaf477300da304ffb3c50af99
JLBegin <ludovick.begin@gmail.com>
2022-02-07 21:44:09 -0500

    Update testMayaviViewer.py

--- pytissueoptics/scene/tests/viewer/testMayaviViewer.py
+++ pytissueoptics/scene/tests/viewer/testMayaviViewer.py
@@ -1,18 +1,36 @@
 import unittest
+from unittest.mock import patch
+
+from pytissueoptics.scene import Cube
+from pytissueoptics.scene.geometry import primitives
+from pytissueoptics.scene.viewer.mayavi import MayaviViewer
 
 
 class TestMayaviViewer(unittest.TestCase):
-    # todo(?): maybe change interface
-    # - move add to private methods
-    # - define showScene(), showSolid()
-    def testWhenAddMayaviSolidWithTrianglePrimitive_shouldAddTheSolid(self):
-        # assert call on mlab.triangular_mesh
-        pass
+    @patch('mayavi.mlab.triangular_mesh')
+    def testWhenAddSolidWithTrianglePrimitive_shouldAddTheSolid(self, fakeMayaviTriangularMesh):
+        viewer = MayaviViewer()
+        solid = Cube(3, primitive=primitives.TRIANGLE)
 
-    def testWhenAddMayaviSolidWithoutTrianglePrimitive_shouldNotAddTheSolid(self):
-        # assert raises
-        pass
+        viewer.add(solid)
 
-    def testWhenShow_shouldDisplayTheMayaviViewer(self):
-        # assert call to mlab.show
-        pass
+        self.assertEqual(1, fakeMayaviTriangularMesh.call_count)
+
+    @patch('mayavi.mlab.triangular_mesh')
+    def testWhenAddSolidWithoutTrianglePrimitive_shouldNotAddTheSolid(self, fakeMayaviTriangularMesh):
+        viewer = MayaviViewer()
+        solid = Cube(3, primitive=primitives.QUAD)
+
+        with self.assertRaises(Exception):
+            viewer.add(solid)
+        self.assertEqual(0, fakeMayaviTriangularMesh.call_count)
+
+    @patch('mayavi.mlab.show')
+    def testWhenShow_shouldDisplayTheMayaviViewer(self, fakeShow):
+        viewer = MayaviViewer()
+        solid = Cube(3, primitive=primitives.TRIANGLE)
+        viewer.add(solid)
+
+        viewer.show()
+
+        self.assertEqual(1, fakeShow.call_count)
