642a4013a2b8fb43d6eba4cc7be5a7bf9aa3da0a
JLBegin <ludovick.begin@gmail.com>
2022-02-09 16:41:01 -0500

    TestEllipsoid: isolate duplicate code

--- pytissueoptics/scene/tests/solids/testEllipsoid.py
+++ pytissueoptics/scene/tests/solids/testEllipsoid.py
@@ -30,13 +30,9 @@ class TestSphere(unittest.TestCase):
 
     def testGivenALowOrderEllipsoid_shouldApproachCorrectEllipsoidAreaTo5Percent(self):
         ellipsoid = Ellipsoid(a=2, b=3, c=5, order=3)
-        p = 8/5
-        a = ellipsoid._a
-        b = ellipsoid._b
-        c = ellipsoid._c
+        perfectEllipsoidArea = self._getPerfectEllipsoidArea(ellipsoid)
         tolerance = 0.05
 
-        perfectEllipsoidArea = 4*math.pi*((a**p*b**p + a**p*c**p + b**p*c**p)/3)**(1/p)
         ellipsoidArea = self._getTotalTrianglesArea(ellipsoid._surfaceDict["noLabel"])
 
         self.assertAlmostEqual(perfectEllipsoidArea, ellipsoidArea, delta=perfectEllipsoidArea*tolerance)
@@ -44,13 +40,9 @@ class TestSphere(unittest.TestCase):
 
     def testGivenAHighOrderEllipsoid_shouldApproachCorrectEllipsoidAreaTo1Percent(self):
         ellipsoid = Ellipsoid(a=2, b=3, c=5, order=6)
-        p = 8 / 5
-        a = ellipsoid._a
-        b = ellipsoid._b
-        c = ellipsoid._c
+        perfectEllipsoidArea = self._getPerfectEllipsoidArea(ellipsoid)
         tolerance = 0.01
 
-        perfectEllipsoidArea = 4 * math.pi * ((a ** p * b ** p + a ** p * c ** p + b ** p * c ** p) / 3) ** (1 / p)
         ellipsoidArea = self._getTotalTrianglesArea(ellipsoid._surfaceDict["noLabel"])
 
         self.assertAlmostEqual(perfectEllipsoidArea, ellipsoidArea, delta=tolerance*perfectEllipsoidArea)
@@ -63,3 +55,11 @@ class TestSphere(unittest.TestCase):
             AC = surface.vertices[0] - surface.vertices[2]
             totalArea += 0.5 * AB.cross(AC).getNorm()
         return totalArea
+
+    @staticmethod
+    def _getPerfectEllipsoidArea(ellipsoid: Ellipsoid) -> float:
+        p = 8 / 5
+        a = ellipsoid._a
+        b = ellipsoid._b
+        c = ellipsoid._c
+        return 4 * math.pi * ((a ** p * b ** p + a ** p * c ** p + b ** p * c ** p) / 3) ** (1 / p)
