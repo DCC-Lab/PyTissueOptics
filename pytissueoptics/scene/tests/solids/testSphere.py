5ced317443c864705cb9bd55ad25eb4535a07759
JLBegin <ludovick.begin@gmail.com>
2022-02-09 16:03:01 -0500

    TestSphere: checkstyle

--- pytissueoptics/scene/tests/solids/testSphere.py
+++ pytissueoptics/scene/tests/solids/testSphere.py
@@ -29,7 +29,6 @@ class TestSphere(unittest.TestCase):
         perfectSphereArea = 4*math.pi*sphere.radius**2
 
         for surface in sphere._surfaceDict["noLabel"]:
-
             icosphereArea += 0.5 * surface.vertices[0].cross(surface.vertices[1]).getNorm()
 
         self.assertNotAlmostEqual(perfectSphereArea, icosphereArea, 3)
@@ -40,7 +39,6 @@ class TestSphere(unittest.TestCase):
         perfectSphereArea = 4 * math.pi * sphere.radius ** 2
 
         for surface in sphere._surfaceDict["noLabel"]:
-
             AB = surface.vertices[0]-surface.vertices[1]
             AC = surface.vertices[0]-surface.vertices[2]
             icosphereArea += 0.5 * AB.cross(AC).getNorm()
