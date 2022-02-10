e84d2942e45a33923121989cbfe70b34f27a957d
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-10 15:06:16 -0500

    add parse test to fill attributes

--- pytissueoptics/scene/tests/loader/parsers/testOBJParser.py
+++ pytissueoptics/scene/tests/loader/parsers/testOBJParser.py
@@ -3,21 +3,25 @@ from pytissueoptics.scene.loader.parsers import OBJParser
 
 
 class TestOBJParser(unittest.TestCase):
-    def testGivenNoFilepath_shouldNotParse(self):
+    def testGivenNoFilepath_shouldRaiseException(self):
         with self.assertRaises(Exception):
             parser = OBJParser()
 
-    def testGivenWrongExtension_shouldNotParse(self):
+    def testGivenWrongExtension_shouldRaiseTypeError(self):
         with self.assertRaises(TypeError):
             parser = OBJParser("objFiles/test.wrongExtension")
 
-    def testShouldParseAtCreation(self):
+    def testAtCreation_ShouldFillAttributes(self):
         parser = OBJParser("objFiles/testCubeQuads.obj")
+        self.assertTrue(parser.objects)
+        self.assertTrue(parser.vertices)
+        self.assertTrue(parser.normals)
 
-    def testShouldGiveCorrectAmountOfVertices(self):
+    def testWithTestCube_ShouldGiveCorrectAmountOfVertices(self):
         parser = OBJParser("objFiles/testCubeQuads.obj")
         self.assertEqual(8, len(parser.vertices))
 
-    def testShouldGiveCorrectAmountOfNormals(self):
+    def testWithTestCube_ShouldGiveCorrectAmountOfNormals(self):
         parser = OBJParser("objFiles/testCubeQuads.obj")
         self.assertEqual(6, len(parser.normals))
+
