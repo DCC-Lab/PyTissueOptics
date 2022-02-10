0bef58544007b48a523364ef91046fae1f639b2d
JLBegin <ludovick.begin@gmail.com>
2022-02-10 15:57:08 -0500

    TestLoader: fix test file paths

--- pytissueoptics/scene/tests/loader/testLoader.py
+++ pytissueoptics/scene/tests/loader/testLoader.py
@@ -1,39 +1,46 @@
+import os
 import unittest
 from typing import List
+
 from pytissueoptics.scene.loader import Loader
 from pytissueoptics.scene.solids import Solid
 
 
 class TestLoader(unittest.TestCase):
+    TEST_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
     def testWhenLoadWithWrongExtension_shouldNotLoad(self):
         with self.assertRaises(ValueError):
-            _ = Loader().load("test.wrongExtension")
+            _ = Loader().load(self._filepath("test.wrongExtension"))
 
     def testWhenLoadingOBJ_shouldLoad(self):
         loader = Loader()
-        _ = loader.load("./parsers/objFiles/droid.obj")
+        _ = loader.load(self._filepath("droid.obj"))
 
     def testWhenLoadingOBJ_shouldReturnListOfSolids(self):
         loader = Loader()
-        solids = loader.load("./parsers/objFiles/droid.obj")
+        solids = loader.load(self._filepath("droid.obj"))
         self.assertIsInstance(solids, List)
         for solid in solids:
             self.assertIsInstance(solid, Solid)
 
     def testWhenLoadingMultiPolygonObject_shouldSplitInTriangles(self):
         loader = Loader()
-        solids = loader.load("./parsers/objFiles/testCubeTrianglesMulti.obj")
+        solids = loader.load(self._filepath("testCubeTrianglesMulti.obj"))
         self.assertEqual(13, len(solids[0].surfaces))
 
     def testWhenLoadingMultiGroupObject_shouldSplitCorrectGroups(self):
         loader = Loader()
-        solids = loader.load("./parsers/objFiles/testCubeTrianglesMulti.obj")
+        solids = loader.load(self._filepath("testCubeTrianglesMulti.obj"))
         self.assertCountEqual(solids[0].groups, ["front", "back", "bottom", "top", "right", "left"])
 
     def testWhenLoadingMultiGroupObject_shouldHaveCorrectAmountOfElementsPerGroup(self):
         loader = Loader()
 
-        solidObjects = loader.load("./parsers/objFiles/testCubeTrianglesMulti.obj")
+        solidObjects = loader.load(self._filepath("testCubeTrianglesMulti.obj"))
 
         self.assertEqual(len(solidObjects[0]._surfaceDict["front"]), 2)
         self.assertEqual(len(solidObjects[0]._surfaceDict["back"]), 3)
+
+    def _filepath(self, fileName) -> str:
+        return os.path.join(self.TEST_DIRECTORY, "parsers", "objFiles", fileName)
