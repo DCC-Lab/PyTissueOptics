ac96a27b838cafd2f79b263296c88d9a6185b1d3
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-09 20:23:54 -0500

    loader: split multivertices polygons

--- pytissueoptics/scene/loaders/loader.py
+++ pytissueoptics/scene/loaders/loader.py
@@ -1,7 +1,8 @@
 import pathlib
+from typing import List
 from .parsers import Parser, OBJParser
 from ..solids.solid import Solid
-from ..geometry import Vector, Polygon
+from ..geometry import Vector, Polygon, Triangle
 
 
 class Loader:
@@ -31,31 +32,48 @@ class Loader:
         else:
             raise ValueError("This format is not supported.")
 
-    def _convert(self):
+    def _convert(self) -> List[Solid]:
         solids = []
-        if len(self._parser.objects) >= 1:
-            vertices = []
-            for vertex in self._parser.vertices:
-                vertices.append(Vector(*vertex))
+        vertices = []
+        for vertex in self._parser.vertices:
+            vertices.append(Vector(*vertex))
 
-            for objectName in self._parser.objects:
-                surfacesGroups = {}
-                for group in self._parser.objects[objectName]["Groups"]:
-                    surfacesGroups[group] = []
-                    for polygonIndices in self._parser.objects[objectName]["Groups"][group]["Polygons"]:
-                        if len(polygonIndices) == 3:
-                            surfacesGroups[group].append(Polygon(vertices=[vertices[polygonIndices[0]], vertices[polygonIndices[1]], vertices[polygonIndices[2]]]))
-                        if len(polygonIndices) == 4:
-                            surfacesGroups[group].append(Polygon(vertices=[vertices[polygonIndices[0]], vertices[polygonIndices[1]], vertices[polygonIndices[2]]]))
-                            surfacesGroups[group].append(Polygon(vertices=[vertices[polygonIndices[0]], vertices[polygonIndices[3]], vertices[polygonIndices[2]]]))
+        for objectName in self._parser.objects:
+            surfacesGroups = {}
 
-                solids.append(Solid(position=Vector(0, 0, 0), vertices=vertices, surfaces=surfacesGroups))
-            return solids
+            for group in self._parser.objects[objectName]["Groups"]:
+                surfacesGroups[group] = []
 
-        else:
-            print("argh.")
+                for polygonIndices in self._parser.objects[objectName]["Groups"][group]["Polygons"]:
 
-    def load(self, filepath):
+                    if len(polygonIndices) == 3:
+                        ai, bi, ci = polygonIndices
+                        surfacesGroups[group].append(Triangle(vertices[ai], vertices[bi], vertices[ci]))
+
+                    elif len(polygonIndices) == 4:
+                        ai, bi, ci, di = polygonIndices
+                        surfacesGroups[group].append(Triangle(vertices[ai], vertices[bi], vertices[ci]))
+                        surfacesGroups[group].append(Triangle(vertices[ai], vertices[ci], vertices[di]))
+
+                    elif len(polygonIndices) > 4:
+                        trianglesIndices = self._splitPolygonIndices(polygonIndices)
+                        print(f"Multipolygon:{len(trianglesIndices)}")
+                        for triangleIndex in trianglesIndices:
+                            ai, bi, ci = triangleIndex
+                            surfacesGroups[group].append(Triangle(vertices[ai], vertices[bi], vertices[ci]))
+
+            solids.append(Solid(position=Vector(0, 0, 0), vertices=vertices, surfaceDict=surfacesGroups))
+
+        return solids
+
+    @staticmethod
+    def _splitPolygonIndices(polygonIndices: List[int]) -> List[List[int]]:
+        trianglesIndices = []
+        for i in range(len(polygonIndices)-2):
+            trianglesIndices.append([polygonIndices[0], polygonIndices[i+1], polygonIndices[i+2]])
+        return trianglesIndices
+
+    def load(self, filepath: str) -> List[Solid]:
         self._filepath = filepath
         self._fileExtension = self._getFileExtension()
         self._selectParser()
