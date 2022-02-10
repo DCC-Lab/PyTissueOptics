cf63129537e6676efe8d998832021657716c29f2
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-09 20:24:28 -0500

    solid: groups property add

--- pytissueoptics/scene/solids/solid.py
+++ pytissueoptics/scene/solids/solid.py
@@ -38,6 +38,10 @@ class Solid:
         return surfaces
 
     @property
+    def groups(self) -> List[str]:
+        return list(self._surfaceDict.keys())
+
+    @property
     def primitive(self) -> str:
         return self._primitive
 
