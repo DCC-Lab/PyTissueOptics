7c23c27c622613d9f11949da5ad643eec0345b3f
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-10 15:22:17 -0500

    parser test for loading components (vertices, normals, index)

--- pytissueoptics/scene/tests/loader/parsers/testOBJParser.py
+++ pytissueoptics/scene/tests/loader/parsers/testOBJParser.py
@@ -21,7 +21,23 @@ class TestOBJParser(unittest.TestCase):
         parser = OBJParser("objFiles/testCubeQuads.obj")
         self.assertEqual(8, len(parser.vertices))
 
+    def testWithTestCube_ShouldGiveCorrectVertices(self):
+        parser = OBJParser("objFiles/testCubeQuads.obj")
+        vertices = [[-0.5, -0.5, -0.5], [0.5, -0.5, -0.5], [0.5, 0.5, -0.5], [-0.5, 0.5, -0.5], [-0.5, -0.5, 0.5],
+                    [0.5, -0.5, 0.5], [0.5, 0.5, 0.5], [-0.5, 0.5, 0.5]]
+        self.assertCountEqual(vertices, parser.vertices)
+
     def testWithTestCube_ShouldGiveCorrectAmountOfNormals(self):
         parser = OBJParser("objFiles/testCubeQuads.obj")
         self.assertEqual(6, len(parser.normals))
 
+    def testWithTestCube_ShouldGiveCorrectNormals(self):
+        parser = OBJParser("objFiles/testCubeQuads.obj")
+        normals = [[0, 0, -1], [1, 0, 0], [0, 0, 1], [-1, 0, 0], [0, 1, 0], [0, -1, 0]]
+        self.assertCountEqual(normals, parser.normals)
+
+    def testWithTestCube_shouldGiveCorrectVerticesIndexForPolygonsWithShiftOf1(self):
+        parser = OBJParser("objFiles/testCubeQuads.obj")
+        # indicesBackOBJFile = [5, 6, 7, 8]
+        indicesBackInParser = [[4, 5, 6, 7]]
+        self.assertCountEqual(indicesBackInParser, parser.objects["cube"].surfaces["back"].polygons)
