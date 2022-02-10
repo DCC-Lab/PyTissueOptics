2d3412cb78aeac6157793f73085fd2dd08c04506
JLBegin <ludovick.begin@gmail.com>
2022-02-10 11:21:55 -0500

    Parser: fix docstring typo

--- pytissueoptics/scene/loader/parsers/parser.py
+++ pytissueoptics/scene/loader/parsers/parser.py
@@ -5,8 +5,8 @@ class Parser:
     """
     Parser base class to parse a file and extract relevant information into a structure.
     Each parser will manage one file type and convey the information into a standard format
-    that will then be converted to the Scene language via the a Loader class. The parser
-    is the step in-between Reading the file and converting it into the correct python objects
+    that will then be converted to the Scene language via the Loader class. The parser
+    is the step in-between reading the file and converting it into the correct python objects
     and its dissociation from the Loader is mainly for clarity.
 
     The standard interface for the parser _object will look like this:
@@ -30,7 +30,7 @@ class Parser:
                 "TexCoords":[]}}
                 }
     }
-    Other components, such as the vertices, dont need to be stored in the dictionnary
+    Other components, such as the vertices, dont need to be stored in the dictionary
     The reason is that it is a global entity that has no complexity and is always needed
     for the conversion later down the line.
     """
