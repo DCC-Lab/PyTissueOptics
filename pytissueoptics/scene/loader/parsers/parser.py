e66885289e6510aa1a8b4645ae3acbe053804cc7
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-10 11:11:53 -0500

    Parser class doc

--- pytissueoptics/scene/loader/parsers/parser.py
+++ pytissueoptics/scene/loader/parsers/parser.py
@@ -3,7 +3,36 @@ from typing import List, Dict
 
 class Parser:
     """
-    Base class to parse a file and extract relevant information.
+    Parser base class to parse a file and extract relevant information into a structure.
+    Each parser will manage one file type and convey the information into a standard format
+    that will then be converted to the Scene language via the a Loader class. The parser
+    is the step in-between Reading the file and converting it into the correct python objects
+    and its dissociation from the Loader is mainly for clarity.
+
+    The standard interface for the parser _object will look like this:
+    {"noObject":{
+        "Material":Material(),
+        "Groups":{
+            "NoGroup":{
+                "Polygons":[], 
+                "Normals":[],
+                "TexCoords":[]}},
+    "objectName":{
+        "Material":Material(),
+        "Groups":{
+            "NoGroup":{
+                "Polygons":[],
+                "Normals":[],
+                "TexCoords":[]}
+            "Group0":{
+                "Polygons":[],
+                "Normals":[],
+                "TexCoords":[]}}
+                }
+    }
+    Other components, such as the vertices, dont need to be stored in the dictionnary
+    The reason is that it is a global entity that has no complexity and is always needed
+    for the conversion later down the line.
     """
 
     def __init__(self, filepath: str):
