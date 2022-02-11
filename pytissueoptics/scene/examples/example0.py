8779176aab51d6df3027b274ae26830950b9406c
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-11 15:05:54 -0500

    example 0 dans la direction souhaitée

--- pytissueoptics/scene/examples/example0.py
+++ pytissueoptics/scene/examples/example0.py
@@ -6,7 +6,7 @@ DESCRIPTION = """  Spawning a Cuboid() of side lenghts = 2,3,1 @(0,0,1) and a Sp
 
 def exampleCode():
     from pytissueoptics.scene import Vector, Cuboid, Sphere, MayaviViewer
-    cuboid = Cuboid(2, 3, 1, position=Vector(0, 0, 1))
+    cuboid = Cuboid(2, 3, 1, position=Vector(1, 0, 0))
     sphere = Sphere(radius=0.1, position=Vector(0, 0, 0))
 
     viewer = MayaviViewer()
