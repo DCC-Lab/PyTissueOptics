0df650a1902bc2ddb6b74ff289a228db16f9ab8a
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-14 11:08:49 -0500

    correct init order because of partial import error

--- pytissueoptics/scene/geometry/bbox.py
+++ pytissueoptics/scene/geometry/bbox.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import List
 from copy import deepcopy
-from pytissueoptics.scene import Vector
+from pytissueoptics.scene.geometry import Vector
 
 
 class BoundingBox:
