3083c372f079e537aed8fa847594d959d944505b
JLBegin <ludovick.begin@gmail.com>
2022-02-14 11:17:54 -0500

    Ellipsoid: change init arguments ordering to fit sphere (radius before order)

--- pytissueoptics/scene/solids/ellipsoid.py
+++ pytissueoptics/scene/solids/ellipsoid.py
@@ -18,7 +18,7 @@ class Ellipsoid(Sphere):
         then .add() or .subtract() the corresponding vector.
     """
 
-    def __init__(self, order: int = 3, a: float = 1, b: float = 1, c: float = 1,
+    def __init__(self, a: float = 1, b: float = 1, c: float = 1, order: int = 3,
                  position: Vector = Vector(0, 0, 0), material: Material = None,
                  primitive: str = primitives.DEFAULT):
 
