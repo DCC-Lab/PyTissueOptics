b1fdb850eb101e2db3c9a72fb3fb52715807a707
JLBegin <ludovick.begin@gmail.com>
2022-02-14 18:15:50 -0500

    Scene: solid insertion dev plan

---
+++ pytissueoptics/scene/tests/testScene.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class TestScene(unittest.TestCase):
+    def testWhenAddingASolid_shouldAddItToTheSceneAtTheDesiredPosition(self):
+        pass
+
+    def testWhenAddingASolidWithNonZeroPositionToADesiredPosition_shouldWarnAboutRepositioning(self):
+        pass
+
+    def testWhenAddingASolidInsideAnotherOne_shouldUpdateOutsideMaterialOfThisSolid(self):
+        # when adding solid, check for intersections (if 2 bbox touches,
+        # assert one is contained in the other and update outside material)
+        pass
+
+    def testWhenAddingASolidOverAnotherOne_shouldUpdateOutsideMaterialOfTheOtherSolid(self):
+        pass
+
+    def testWhenAddingASolidThatPartlyMergesWithAnotherOne_shouldNotAdd(self):
+        pass
+
+    def testWhenAddingASolidInsideACuboidStack_shouldRaiseNotImplementedError(self):
+        pass
