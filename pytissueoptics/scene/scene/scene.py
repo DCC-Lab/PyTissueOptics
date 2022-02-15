d27c4276880262b6ebbe78427bd7ec2b70ca53e4
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-15 10:53:05 -0500

    scene: getPolygons + getBoundingBox

--- pytissueoptics/scene/scene/scene.py
+++ pytissueoptics/scene/scene/scene.py
@@ -1,8 +1,22 @@
 from typing import List
 
 from pytissueoptics.scene.solids import Solid
+from pytissueoptics.scene.geometry import Polygon, BoundingBox
 
 
 class Scene:
     def __init__(self, solids: List[Solid]):
-        self.solids = solids
+        self._solids = solids
+        self._boundingBox = None
+
+    def getPolygons(self) -> List[Polygon]:
+        polygons = []
+        for solid in self._solids:
+            polygons.extend(solid.surfaces.getPolygons())
+        return polygons
+
+    def getBoundingBox(self) -> BoundingBox:
+        bbox = BoundingBox(xLim=[0, 0], yLim=[0, 0], zLim=[0, 0])
+        for solid in self._solids:
+            bbox.upscaleTo(solid.bbox)
+        return bbox
