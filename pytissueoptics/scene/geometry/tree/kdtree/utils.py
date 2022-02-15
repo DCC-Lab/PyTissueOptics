3e6280ac3df5518d35edb1a97073127f147c6228
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-15 11:06:13 -0500

    correct splitter utilities

--- pytissueoptics/scene/geometry/tree/kdtree/utils.py
+++ pytissueoptics/scene/geometry/tree/kdtree/utils.py
@@ -2,26 +2,27 @@ from pytissueoptics.scene.geometry.tree.kdtree import KDNode
 
 
 class Splitter:
-    def calculateSplitLine(self, node: KDNode):
-        pass
+    def calculateSplitLine(self, node: KDNode) -> float:
+        raise NotImplementedError
 
 
 class CentroidSplitter(Splitter):
-    def calculateSplitLine(self, node: KDNode):
+    def calculateSplitLine(self, node: KDNode) -> float:
         average = 0
-        for triangle in node.triangles:
+        for polygon in node.polygons:
             if node.axis == "x":
-                average += triangle.globalCentroid.x
+                average += polygon.centroid.x
             elif node.axis == "y":
-                average += triangle.globalCentroid.y
+                average += polygon.centroid.y
             elif node.axis == "z":
-                average += triangle.globalCentroid.z
-        splitLine = average / len(node.triangles)
-        return splitLine
+                average += polygon.centroid.z
+
+        average = average / len(node.polygons)
+        return average
 
 
 class BinarySplitter(Splitter):
-    def calculateSplitLine(self, node):
-        boundaryMin = node.boundingBox[node.axis.axis].min
-        boundaryMax = node.boundingBox[node.axis.axis].max
-        return boundaryMin + (boundaryMax-boundaryMin)/2
+    def calculateSplitLine(self, node: KDNode) -> float:
+        boundaryMin = node.boundingBox.getAxisLimit(node.axis, "min")
+        boundaryMax = node.boundingBox.getAxisLimit(node.axis, "max")
+        return (boundaryMin + boundaryMax) / 2
