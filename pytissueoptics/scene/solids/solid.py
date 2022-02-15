2e75734e6dce55384a4fc122c552bc2ded1ae93b
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-15 14:01:32 -0500

    resetPolygonsCentroids

--- pytissueoptics/scene/solids/solid.py
+++ pytissueoptics/scene/solids/solid.py
@@ -24,7 +24,8 @@ class Solid:
 
         self.translateTo(position)
         self._setInsideMaterial()
-        self._resetBoundingBox()
+        self._resetBoundingBoxes()
+        self._resetPolygonsCentroids()
 
     @property
     def position(self) -> Vector:
@@ -46,9 +47,12 @@ class Solid:
     def bbox(self) -> BoundingBox:
         return self._bbox
 
-    def _resetBoundingBox(self):
+    def _resetBoundingBoxes(self):
         self._bbox = BoundingBox.fromVertices(self._vertices)
         self._surfaces.resetBoundingBoxes()
+
+    def _resetPolygonsCentroids(self):
+        self._surfaces.resetCentroids()
 
     def translateTo(self, position):
         if position == self._position:
@@ -60,7 +64,8 @@ class Solid:
         self._position.add(translationVector)
         for v in self._vertices:
             v.add(translationVector)
-        self._resetBoundingBox()
+        self._resetBoundingBoxes()
+        self.resetPolygonsCentroids()
 
     def rotate(self, xTheta=0, yTheta=0, zTheta=0):
         """
@@ -80,9 +85,10 @@ class Solid:
         for (vertex, rotatedVertexArray) in zip(self._vertices, rotatedVerticesArray):
             vertex.update(*rotatedVertexArray)
 
-        self._surfaces.resetNormals()
         self._orientation.add(rotation)
-        self._resetBoundingBox()
+        self._surfaces.resetNormals()
+        self._resetBoundingBoxes()
+        self._resetPolygonsCentroids()
 
     def getMaterial(self, surfaceName: str = None) -> Material:
         if surfaceName:
