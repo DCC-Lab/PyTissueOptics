145f0495b1895f48d204c12250e3c9e7b0faf604
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-14 20:10:37 -0500

    tests: delete get tests

--- pytissueoptics/scene/tests/geometry/testBoundingBox.py
+++ pytissueoptics/scene/tests/geometry/testBoundingBox.py
@@ -19,33 +19,6 @@ class TestBoundingBox(unittest.TestCase):
         with self.assertRaises(ValueError):
             _ = BoundingBox(self.xLim, self.yLim, self.zLim)
 
-    def testGivenLimits_whenAskingLimits_shouldReturnCorrectLimits(self):
-        bbox = BoundingBox(self.xLim, self.yLim, self.zLim)
-
-        self.assertEqual(bbox.xLim, self.xLim)
-        self.assertEqual(bbox.yLim, self.yLim)
-        self.assertEqual(bbox.zLim, self.zLim)
-
-    def testGivenLimits_whenAskingMinMax_shouldReturnMinMax(self):
-        bbox = BoundingBox(self.xLim, self.yLim, self.zLim)
-
-        self.assertEqual(bbox.xMin, self.xLim[0])
-        self.assertEqual(bbox.xMax, self.xLim[1])
-        self.assertEqual(bbox.yMin, self.yLim[0])
-        self.assertEqual(bbox.yMax, self.yLim[1])
-        self.assertEqual(bbox.zMin, self.zLim[0])
-        self.assertEqual(bbox.zMax, self.zLim[1])
-
-    def testGivenNewBBoxFromVertices_whenAskingLimits_shouldReturnCorrectLimits(self):
-        v1 = Vector(0, 1, 0)
-        v2 = Vector(-1, 1, 2)
-        v3 = Vector(-0.1, -1, 3.001)
-        bbox = BoundingBox.fromVertices([v1, v2, v3])
-
-        self.assertEqual(bbox.xLim, [-1, 0])
-        self.assertEqual(bbox.yLim, [-1, 1])
-        self.assertEqual(bbox.zLim, [0, 3.001])
-
     def testGiven2SimilarBBox_whenEquals_shouldReturnTrue(self):
         bbox1 = BoundingBox(self.xLim, self.yLim, self.zLim)
         bbox2 = BoundingBox(self.xLim, self.yLim, self.zLim)
