a8066156e1c5f2d4fdd8cbbcfee53a987783290a
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-14 20:53:08 -0500

    test: re-add bbox from vertices test

--- pytissueoptics/scene/tests/geometry/testBoundingBox.py
+++ pytissueoptics/scene/tests/geometry/testBoundingBox.py
@@ -28,3 +28,13 @@ class TestBoundingBox(unittest.TestCase):
         bbox1 = BoundingBox(self.xLim, self.yLim, self.zLim)
         bbox2 = BoundingBox([0, 1.001], self.yLim, self.zLim)
         self.assertTrue(bbox1 != bbox2)
+
+    def testGivenNewBBoxFromVertices_shouldDefineBoundingBoxAroundVertices(self):
+        v1 = Vector(0, 1, 0)
+        v2 = Vector(-1, 1, 2)
+        v3 = Vector(-0.1, -1, 3.001)
+        bbox = BoundingBox.fromVertices([v1, v2, v3])
+
+        self.assertEqual(bbox.xLim, [-1, 0])
+        self.assertEqual(bbox.yLim, [-1, 1])
+        self.assertEqual(bbox.zLim, [0, 3.001])
