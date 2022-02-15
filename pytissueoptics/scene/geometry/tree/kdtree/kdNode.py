f30118e561d7b6e2335d5c88b3560d056c3dfbde
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-15 00:00:39 -0500

    copy kdnode and kdtree parsed functions from previous code

--- pytissueoptics/scene/geometry/tree/kdtree/kdNode.py
+++ pytissueoptics/scene/geometry/tree/kdtree/kdNode.py
@@ -1,2 +1,81 @@
+from __future__ import annotations
+from kd3ba.Axis import Axis
+
+
 class KDNode:
-    pass
+    def __init__(self, parent=None, left=None, right=None, depth=0, axis=Axis("x"), triangles=None, boundingBox=None,
+                 maxDepth=100, splitStrategy=None):
+        self._parent = parent
+        self._left = left
+        self._right = right
+        self._depth = depth
+        self._axis = axis
+        self._maxDepth = maxDepth
+        self._triangles = triangles
+        self._boundingBox = boundingBox
+        self._splitStrategy = splitStrategy
+        self.split()
+
+    @property
+    def isRoot(self):
+        if self._parent is None:
+            return True
+        else:
+            return False
+
+    @property
+    def triangles(self):
+        return self._triangles
+
+    @property
+    def axis(self):
+        return self._axis
+
+    @property
+    def boundingBox(self):
+        return self._boundingBox
+
+    def split(self):
+        if self._depth < self._maxDepth and len(self._triangles) > 2:
+            splitLine = self.calculateSplitLine()
+            goingLeft, goingRight = self.countLeftRight(splitLine)
+            self._left = KDNode(parent=self, triangles=goingLeft,
+                                boundingBox=self._boundingBox.updateFrom(self._axis.axis, "max", splitLine),
+                                axis=self._axis.nextAxisRotate(), depth=self._depth + 1, maxdepth=self._maxdepth,
+                                splitStrategy=self._splitStrategy)
+            self._right = KDNode(parent=self, triangles=goingRight,
+                                 boundingBox=self._boundingBox.updateFrom(self._axis.axis, "min", splitLine),
+                                 axis=self._axis.nextAxisRotate(), depth=self._depth + 1, maxdepth=self._maxdepth,
+                                 splitStrategy=self._splitStrategy)
+
+    def countLeftRight(self, line=None):
+        goingLeft = []
+        goingRight = []
+
+        for triangle in self._triangles:
+            if triangle.globalBoundingBox[self._axis.axis]["min"] < line and \
+                    triangle.globalBoundingBox[self._axis.axis]["max"] < line:
+                goingLeft.append(triangle)
+            elif triangle.globalBoundingBox[self._axis.axis]["min"] > line and \
+                    triangle.globalBoundingBox[self._axis.axis]["max"] > line:
+                goingRight.append(triangle)
+            else:
+                goingLeft.append(triangle)
+                goingRight.append(triangle)
+
+        return goingLeft, goingRight
+
+    def calculateSplitLine(self):
+        return self._splitStrategy.calculateSplitLine(self)
+
+    def getAllBBox(self, node, bboxList=None):
+        if bboxList is None:
+            bboxList = []
+
+        if node is not None:
+            bboxList.append(node._boundingBox)
+            node.getAllBBox(node._left, bboxList)
+            node.getAllBBox(node._right, bboxList)
+
+            if node.isRoot:
+                return bboxList
--- pytissueoptics/scene/geometry/tree/kdtree/kdTree.py
+++ pytissueoptics/scene/geometry/tree/kdtree/kdTree.py
@@ -1,2 +1,40 @@
+from typing import List
+from kd3ba.KDNode import KDNode
+from kd3ba.Polygon import Triangle
+from kd3ba.BoundingBox import BoundingBox
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product, combinations
+
+
 class KDTree:
-    pass
+    def __init__(self, maxBoundingBox: BoundingBox, objectsList: List[Triangle], maxDepth=100, splitStrategy=None):
+        self._maxBoundingBox = maxBoundingBox
+        self._objectsList = objectsList
+        self._maxDepth = maxDepth
+        self._splitStrategy = splitStrategy
+        self._tree = KDNode(triangles=objectsList, maxDepth=self._maxDepth, boundingBox=self._maxBoundingBox,
+                            splitStrategy=self._splitStrategy)
+
+    def getAllBBox(self):
+        bboxList = self._tree.getAllBBox(self._tree)
+        return bboxList
+
+    def drawTree(self):
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_box_aspect((1, 1, 1))
+
+        # draw kdTree as cube lines
+        for bbox in self.getAllBBox():
+            x = [bbox.xLim.min, bbox.xLim.max]
+            y = [bbox.yLim.min, bbox.yLim.max]
+            z = [bbox.zLim.min, bbox.zLim.max]
+
+            allPossibleBoundaryPoints = np.array(list(product(x, y, z)))
+            allPossibleBoundaryLines = combinations(allPossibleBoundaryPoints, 2)
+            for i, (pointA, pointB) in enumerate(allPossibleBoundaryLines):
+                difference = (pointA - pointB)
+                nonZeroCoordinateDifference = list(filter(lambda a: a != 0, difference))
+                if len(nonZeroCoordinateDifference) == 1:
+                    ax.plot3D(*zip(pointA, pointB), color="k", linewidth=0.25)
