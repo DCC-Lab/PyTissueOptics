4a54a74f6768bced638487873ad7563beb87c587
JLBegin <ludovick.begin@gmail.com>
2022-02-14 23:57:15 -0500

    Scene.add: basic full solid insertion algorithm

--- pytissueoptics/scene/scene/scene.py
+++ pytissueoptics/scene/scene/scene.py
@@ -1,8 +1,44 @@
 from typing import List
 
+from pytissueoptics.scene.geometry import Vector
 from pytissueoptics.scene.solids import Solid
 
 
 class Scene:
-    def __init__(self, solids: List[Solid]):
-        self.solids = solids
+    def __init__(self):
+        self._solids = []
+
+    def add(self, solid: Solid, position: Vector = None):
+        if position:
+            solid.translateTo(position)
+        self._validate(solid)
+        self._solids.append(solid)
+
+    def _validate(self, solid: Solid):
+        if len(self._solids) == 0:
+            return
+
+        intersectingSuspects = self._findIntersectingSuspectsFor(solid)
+        if len(intersectingSuspects) == 0:
+            return
+        if len(intersectingSuspects) != 1:
+            raise NotImplementedError("Cannot handle placement of a solid that intersects with more than one solid. ")
+
+        otherSolid = intersectingSuspects[0]
+        # fixme: or change for more precise/slower contains(solid.vertices)
+        if solid.contains(*otherSolid.getBoundingBox().getVertices()):
+            otherSolid.setOutsideMaterial(solid.getMaterial())
+        elif otherSolid.contains(*solid.getBoundingBox().getVertices()):
+            solid.setOutsideMaterial(otherSolid.getMaterial())
+        else:
+            raise NotImplementedError("Cannot place a solid that partially intersects with an existing solid. ")
+
+    def _findIntersectingSuspectsFor(self, solid) -> List[Solid]:
+        solidBBox = solid.getBoundingBox()
+        intersectingSuspects = self._solids
+        for axis in range(3):
+            for suspect in intersectingSuspects:
+                suspectBBox = suspect.getBoundingBox()
+                if suspectBBox[axis][0] > solidBBox[axis][1] or suspectBBox[axis][1] < solidBBox[axis][0]:
+                    intersectingSuspects.remove(suspect)
+        return intersectingSuspects
--- pytissueoptics/scene/tests/scene/testScene.py
+++ pytissueoptics/scene/tests/scene/testScene.py
@@ -1,22 +1,66 @@
 import unittest
 
+from mockito import mock, verify, when
+
+from pytissueoptics.scene import Material
+from pytissueoptics.scene.scene import Scene
+from pytissueoptics.scene.geometry import Vector, BoundingBox
+from pytissueoptics.scene.solids import Solid
+
 
 class TestScene(unittest.TestCase):
-    def testWhenAddingASolid_shouldAddItToTheSceneAtTheDesiredPosition(self):
-        pass
+    THE_POSITION = Vector(4, 0, 1)
 
-    def testWhenAddingASolidWithNonZeroPositionToADesiredPosition_shouldWarnAboutRepositioning(self):
-        pass
+    def setUp(self):
+        self.scene = Scene()
+
+    def testWhenAddingASolidAtAPosition_shouldPlaceTheSolidAtTheDesiredPosition(self):
+        THE_SOLID = mock(Solid)
+        when(THE_SOLID).translateTo(...).thenReturn()
+
+        self.scene.add(THE_SOLID, position=self.THE_POSITION)
+
+        verify(THE_SOLID).translateTo(self.THE_POSITION)
+
+    def testWhenAddingASolidAtNoSpecificPosition_shouldKeepTheSolidAtItsPredefinedPosition(self):
+        THE_SOLID = mock(Solid)
+        when(THE_SOLID).translateTo(...).thenReturn()
+
+        self.scene.add(THE_SOLID)
+
+        verify(THE_SOLID, times=0).translateTo(...)
+
+    def testWhenAddingASolidThatPartlyOverlapsWithAnotherOne_shouldNotAdd(self):
+        otherBBox = BoundingBox([2, 5], [2, 5], [2, 5])
+        OTHER_SOLID = mock(Solid, {"bbox": otherBBox})
+        solidBBox = BoundingBox([0, 3], [0, 3], [0, 3])
+        THE_SOLID = mock(Solid, {"bbox": solidBBox})
+        self.scene.add(OTHER_SOLID)
+
+        with self.assertRaises(Exception):
+            self.scene.add(THE_SOLID)
 
     def testWhenAddingASolidInsideAnotherOne_shouldUpdateOutsideMaterialOfThisSolid(self):
-        # when adding solid, check for intersections (if 2 bbox touches,
-        # assert one is contained in the other and update outside material)
-        pass
+        OTHER_SOLID = mock(Solid)
+        otherBBox = BoundingBox([0, 5], [0, 5], [0, 5])
+        otherMaterial = Material()
+        when(OTHER_SOLID).getBoundingBox().thenReturn(otherBBox)
+        when(OTHER_SOLID).getMaterial().thenReturn(otherMaterial)
+        self.scene.add(OTHER_SOLID)
+
+        THE_SOLID = mock(Solid)
+        solidBBox = BoundingBox([1, 3], [1, 3], [1, 3])
+        when(THE_SOLID).getBoundingBox().thenReturn(solidBBox)
+        when(THE_SOLID).setOutsideMaterial(...).thenReturn()
+
+        when(OTHER_SOLID).contains(...).thenReturn(True)
+        when(THE_SOLID).contains(...).thenReturn(False)
+
+        self.scene.add(THE_SOLID)
+
+        verify(THE_SOLID).setOutsideMaterial(otherMaterial)
 
     def testWhenAddingASolidOverAnotherOne_shouldUpdateOutsideMaterialOfTheOtherSolid(self):
-        pass
-
-    def testWhenAddingASolidThatPartlyMergesWithAnotherOne_shouldNotAdd(self):
         pass
 
     def testWhenAddingASolidInsideACuboidStack_shouldRaiseNotImplementedError(self):
