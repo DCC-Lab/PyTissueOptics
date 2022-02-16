1d862505a1c17dbe3230684ac6b4cb9ca12ab426
JLBegin <ludovick.begin@gmail.com>
2022-02-16 18:51:46 -0500

    TestTriangleIntersect: test all strategies at once with DDT

--- pytissueoptics/scene/tests/intersection/testTriangleIntersect.py
+++ pytissueoptics/scene/tests/intersection/testTriangleIntersect.py
@@ -1,39 +1,47 @@
 import unittest
 
+from ddt import ddt, data
+
 from pytissueoptics.scene.geometry import Triangle, Vector
 from pytissueoptics.scene.intersection import Ray
 from pytissueoptics.scene.intersection.triangleIntersect import MollerTrumboreIntersect
 
 
-class TestMollerTrumboreIntersect(unittest.TestCase):
-    def testGivenIntersectingRayAndTriangle_shouldReturnIntersectionPoint(self):
-        triangle = Triangle(Vector(0+2, 0, 0), Vector(1+2, 0, 0), Vector(0+2, 1, 0))
-        rayOrigin = Vector(0.25+2, 0.25, 2)
+@ddt
+class TestTriangleIntersect(unittest.TestCase):
+    intersectStrategies = [MollerTrumboreIntersect]
+
+    @data(*intersectStrategies)
+    def testGivenIntersectingRayAndTriangle_shouldReturnIntersectionPoint(self, IntersectStrategy):
+        triangle = Triangle(Vector(0 + 2, 0, 0), Vector(1 + 2, 0, 0), Vector(0 + 2, 1, 0))
+        rayOrigin = Vector(0.25 + 2, 0.25, 2)
         rayDirection = Vector(0.1, 0, -1)
         ray = Ray(rayOrigin, rayDirection)
 
-        intersection = MollerTrumboreIntersect().getIntersection(ray, triangle)
+        intersection = IntersectStrategy().getIntersection(ray, triangle)
 
         self.assertEqual(2.45, intersection.x)
         self.assertEqual(0.25, intersection.y)
         self.assertEqual(0.0, intersection.z)
 
-    def testGivenNonIntersectingRayAndTriangle_shouldReturnNone(self):
+    @data(*intersectStrategies)
+    def testGivenNonIntersectingRayAndTriangle_shouldReturnNone(self, IntersectStrategy):
         triangle = Triangle(Vector(0, 0, 0), Vector(1, 0, 0), Vector(0, 1, 0))
         rayOrigin = Vector(0.25, 0.25, 1)
         rayDirection = Vector(-0.3, 0, -1)
         ray = Ray(rayOrigin, rayDirection)
 
-        intersection = MollerTrumboreIntersect().getIntersection(ray, triangle)
+        intersection = IntersectStrategy().getIntersection(ray, triangle)
 
         self.assertIsNone(intersection)
 
-    def testGivenLineIntersectingRayAndTriangle_shouldReturnNone(self):
+    @data(*intersectStrategies)
+    def testGivenLineIntersectingRayAndTriangle_shouldReturnNone(self, IntersectStrategy):
         triangle = Triangle(Vector(0, 0, 0), Vector(1, 0, 0), Vector(0, 1, 0))
         rayOrigin = Vector(-1, -1, 0)
         rayDirection = Vector(1, 1, 0)
         ray = Ray(rayOrigin, rayDirection)
 
-        intersection = MollerTrumboreIntersect().getIntersection(ray, triangle)
+        intersection = IntersectStrategy().getIntersection(ray, triangle)
 
         self.assertIsNone(intersection)
