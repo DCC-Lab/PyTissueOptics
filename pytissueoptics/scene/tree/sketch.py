af017b733cc63e2547b89b099687e415359a40a5
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-16 22:23:38 -0500

    utils.py for meanCentroid calculation

--- pytissueoptics/scene/tree/sketch.py
+++ pytissueoptics/scene/tree/sketch.py
@@ -1,17 +1,20 @@
 from pytissueoptics.scene.scene import Scene
 from pytissueoptics.scene.geometry import Vector
 from pytissueoptics.scene.solids import Cuboid, Sphere
+from pytissueoptics.scene.loader import Loader
+
 from pytissueoptics.scene.tree.binary.binaryTreeStrategy import BasicKDTreeStrategy
 from pytissueoptics.scene.tree.binary import BinaryTree
 from pytissueoptics.scene.viewer import MayaviViewer
 import time
 
-cuboid1 = Cuboid(a=1, b=3, c=1, position=Vector(1, 0, 0))
+cuboid1 = Cuboid(a=1, b=3, c=1, position=Vector(4, -2, 6))
 cuboid2 = Cuboid(1, 1, 1, position=Vector(-2, -2, 0))
 sphere = Sphere(position=Vector(3, 3, 3), order=4)
-scene = Scene([cuboid1, cuboid2, sphere])
+object1 = Loader().load(filepath="C:\\Users\\marc-\\Documents\\Github\\PyTissueOptics\\pytissueoptics\\scene\\examples\\exampleFile.obj")
+scene = Scene([cuboid1, cuboid2, sphere, *object1])
 t0 = time.time()
-kdTree = BinaryTree(scene=scene, treeStrategy=BasicKDTreeStrategy(), maxDepth=7)
+kdTree = BinaryTree(scene=scene, treeStrategy=BasicKDTreeStrategy(), maxDepth=14)
 t1 = time.time()
 print(t1-t0)
 
@@ -19,6 +22,6 @@ bboxes = kdTree.getLeafBoundingBoxesAsCuboids()
 print(len(bboxes))
 
 viewer = MayaviViewer()
-viewer.add(*scene.getSolids(), representation="mesh")
+viewer.add(*scene.getSolids(), representation="mesh", lineWidth=1)
 viewer.add(*bboxes, representation="surface", lineWidth=2, opacity=0.25)
 viewer.show()
