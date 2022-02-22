5849f060af99db41019d9ba9ba0e55eef5622977
JLBegin <ludovick.begin@gmail.com>
2022-02-22 11:10:30 -0500

    Logger: remove unused methods

--- pytissueoptics/scene/logger/logger.py
+++ pytissueoptics/scene/logger/logger.py
@@ -35,24 +35,6 @@ class Logger:
     def segments(self):
         return self._segments
 
-    def getMinDataPoint(self) -> DataPoint:
-        minDataPoint = None
-        minValue = sys.maxsize
-        for dataPoint in self._dataPoints:
-            if dataPoint.value < minValue:
-                minValue = dataPoint.value
-                minDataPoint = dataPoint
-        return minDataPoint
-
-    def getMaxDataPoint(self) -> DataPoint:
-        maxDataPoint = None
-        maxValue = -sys.maxsize
-        for dataPoint in self._dataPoints:
-            if dataPoint.value > maxValue:
-                maxValue = dataPoint.value
-                maxDataPoint = dataPoint
-        return maxDataPoint
-
     def logPoint(self, point: Vector):
         self._points.append(point)
 
--- pytissueoptics/scene/tests/logger/testLogger.py
+++ pytissueoptics/scene/tests/logger/testLogger.py
@@ -44,25 +44,3 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(3, len(logger.segments))
         self.assertEqual(Vector(0, 0, 0), logger.segments[-1].start)
         self.assertEqual(Vector(1, 0, 0), logger.segments[-1].end)
-
-    def testGivenLoggerWithMultipleDataPoints_whenGetMinDataPoint_shouldReturnDataPointWithMinimumValue(self):
-        logger = Logger()
-        logger.logDataPoint(-10.5, Vector(0, 0, 0))
-        logger.logDataPoint(10, Vector(2, 0, 0))
-        logger.logDataPoint(20.5, Vector(1, 0, 0))
-
-        minDataPoint = logger.getMinDataPoint()
-
-        self.assertEqual(-10.5, minDataPoint.value)
-        self.assertEqual(Vector(0, 0, 0), minDataPoint.position)
-
-    def testGivenLoggerWithMultipleDataPoints_whenGetMaxDataPoint_shouldReturnDataPointWithMaximumValue(self):
-        logger = Logger()
-        logger.logDataPoint(-10.5, Vector(0, 0, 0))
-        logger.logDataPoint(10, Vector(2, 0, 0))
-        logger.logDataPoint(20.5, Vector(1, 0, 0))
-
-        maxDataPoint = logger.getMaxDataPoint()
-
-        self.assertEqual(20.5, maxDataPoint.value)
-        self.assertEqual(Vector(1, 0, 0), maxDataPoint.position)
