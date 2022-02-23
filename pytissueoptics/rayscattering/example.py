036e2edfbf7c8eb092ac0f74b9d1bed572e43a64
JLBegin <ludovick.begin@gmail.com>
2022-02-23 16:11:58 -0500

    Add example with current scattering simulation : no reflections, no refraction angle and no scattering angle.

---
+++ pytissueoptics/rayscattering/example.py
@@ -0,0 +1,21 @@
+from pytissueoptics.rayscattering.source import Source
+from pytissueoptics.rayscattering.tissues import PhantomTissue
+from pytissueoptics.scene import MayaviViewer, Vector, Material
+from pytissueoptics.scene.logger import Logger
+import numpy as np
+
+np.random.seed(11)
+
+viewer = MayaviViewer()
+tissue = PhantomTissue()
+# tissue.display()
+
+tissue.addToViewer(viewer)
+
+logger = Logger()
+source = Source(position=Vector(0, 0, -1))
+
+source.propagate(tissue, logger=logger)
+
+viewer.addLogger(logger)
+viewer.show()
