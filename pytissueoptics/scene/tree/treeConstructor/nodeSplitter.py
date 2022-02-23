865681f36e1e00dc7af2685b063bd84766fe6d92
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-02-23 11:04:35 -0500

    return to static method

--- pytissueoptics/scene/tree/treeConstructor/nodeSplitter.py
+++ pytissueoptics/scene/tree/treeConstructor/nodeSplitter.py
@@ -12,13 +12,10 @@ class NodeSplitter:
     def run(self, splitAxis: str, nodeBbox: BoundingBox, polygons: List[Polygon]) -> SplitNodeResult:
         raise NotImplementedError
 
-    def _getNewChildrenBbox(self, nodeBbox, splitAxis, splitLine) -> List[BoundingBox]:
-        leftBbox = self._copyBboxThenUpdate(nodeBbox, splitAxis, "max", splitLine)
-        rightBbox = self._copyBboxThenUpdate(nodeBbox, splitAxis, "min", splitLine)
-        return [leftBbox, rightBbox]
-
     @staticmethod
-    def _copyBboxThenUpdate(bbox: BoundingBox, axis: str, limit: str, value: float) -> BoundingBox:
-        newBbox = bbox.copy()
-        newBbox.update(axis, limit, value)
-        return newBbox
+    def _getNewChildrenBbox(nodeBbox, splitAxis, splitLine) -> List[BoundingBox]:
+        leftBbox = nodeBbox.copy()
+        leftBbox.update(splitAxis, "max", splitLine)
+        rightBbox = nodeBbox.copy()
+        rightBbox.update(splitAxis, "min", splitLine)
+        return [leftBbox, rightBbox]
