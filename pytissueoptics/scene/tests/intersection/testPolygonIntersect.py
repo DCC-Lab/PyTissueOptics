84c916c1483dbeff57bc44e6b2ed2ac9dce38eaa
JLBegin <ludovick.begin@gmail.com>
2022-02-25 15:53:29 -0500

    update TestPolygonIntersect: use inheritance to test strategy instead of DDT

--- pytissueoptics/scene/tests/intersection/testPolygonIntersect.py
+++ pytissueoptics/scene/tests/intersection/testPolygonIntersect.py
@@ -1,81 +1,94 @@
 import unittest
 
-from ddt import ddt, data
-
-from pytissueoptics.scene.geometry import Triangle, Vector, Quad
+from pytissueoptics.scene.geometry import Triangle, Vector, Quad, Polygon
 from pytissueoptics.scene.intersection import Ray
 from pytissueoptics.scene.intersection.quadIntersect import MollerTrumboreQuadIntersect
 from pytissueoptics.scene.intersection.triangleIntersect import MollerTrumboreTriangleIntersect
 
 
-@ddt
-class TestPolygonIntersect(unittest.TestCase):
-    intersectStrategies = [(MollerTrumboreTriangleIntersect,
-                            Triangle(Vector(0, 0, 0), Vector(1, 0, 0), Vector(0, 1, 0))),
-                           (MollerTrumboreQuadIntersect,
-                            Quad(Vector(0, 0, 0), Vector(1, 0, 0), Vector(1, 1, 0), Vector(0, 1, 0)))]
+class TestAnyPolygonIntersect:
+    vertices = [Vector(0, 0, 0), Vector(1, 0, 0), Vector(1, 1, 0), Vector(0, 1, 0)]
 
-    @data(*intersectStrategies)
-    def testGivenIntersectingRayAndPolygon_shouldReturnIntersectionPosition(self, anyStrategyAndPolygonPair):
-        AnyIntersectStrategy, polygon = anyStrategyAndPolygonPair
+    @property
+    def intersectStrategy(self):
+        # todo: we need a IntersectStrategy interface before box and polygon (or triangle, quad) intersect
+        raise NotImplementedError
+
+    @property
+    def aPolygon(self) -> Polygon:
+        raise NotImplementedError
+
+    def testGivenIntersectingRayAndPolygon_shouldReturnIntersectionPosition(self):
         rayOrigin = Vector(0.25, 0.25, 2)
         rayDirection = Vector(0.1, 0, -1)
         rayDirection.normalize()
         ray = Ray(rayOrigin, rayDirection)
 
-        intersection = AnyIntersectStrategy().getIntersection(ray, polygon)
+        intersection = self.intersectStrategy.getIntersection(ray, self.aPolygon)
 
         self.assertEqual(0.45, intersection.x)
         self.assertEqual(0.25, intersection.y)
         self.assertEqual(0.0, intersection.z)
 
-    @data(*intersectStrategies)
-    def testGivenNonIntersectingRayAndPolygon_shouldReturnNone(self, anyStrategyAndPolygonPair):
-        AnyIntersectStrategy, polygon = anyStrategyAndPolygonPair
+    def testGivenNonIntersectingRayAndPolygon_shouldReturnNone(self):
         rayOrigin = Vector(0.25, 0.25, 1)
         rayDirection = Vector(-0.3, 0, -1)
         rayDirection.normalize()
         ray = Ray(rayOrigin, rayDirection)
 
-        intersection = AnyIntersectStrategy().getIntersection(ray, polygon)
+        intersection = self.intersectStrategy.getIntersection(ray, self.aPolygon)
 
         self.assertIsNone(intersection)
 
-    @data(*intersectStrategies)
-    def testGivenLineIntersectingRayAndPolygon_shouldReturnNone(self, anyStrategyAndPolygonPair):
-        AnyIntersectStrategy, polygon = anyStrategyAndPolygonPair
+    def testGivenLineIntersectingRayAndPolygon_shouldReturnNone(self):
         rayOrigin = Vector(-1, -1, 0)
         rayDirection = Vector(1, 1, 0)
         rayDirection.normalize()
         ray = Ray(rayOrigin, rayDirection)
 
-        intersection = AnyIntersectStrategy().getIntersection(ray, polygon)
+        intersection = self.intersectStrategy.getIntersection(ray, self.aPolygon)
 
         self.assertIsNone(intersection)
 
-    @data(*intersectStrategies)
-    def testGivenRayShorterThanPolygonIntersectionDistance_shouldReturnNone(self, anyStrategyAndPolygonPair):
-        AnyIntersectStrategy, polygon = anyStrategyAndPolygonPair
+    def testGivenRayShorterThanPolygonIntersectionDistance_shouldReturnNone(self):
         rayOrigin = Vector(0.25, 0.25, 2)
         rayDirection = Vector(0.1, 0, -1)
         rayDirection.normalize()
         ray = Ray(rayOrigin, rayDirection, length=1.8)
 
-        intersection = AnyIntersectStrategy().getIntersection(ray, polygon)
+        intersection = self.intersectStrategy.getIntersection(ray, self.aPolygon)
 
         self.assertIsNone(intersection)
 
-    @data(*intersectStrategies)
-    def testGivenRayLongerThanPolygonIntersectionDistance_shouldReturnIntersectionPosition(self, anyStrategyAndPolygonPair):
-        AnyIntersectStrategy, polygon = anyStrategyAndPolygonPair
+    def testGivenRayLongerThanPolygonIntersectionDistance_shouldReturnIntersectionPosition(self):
         rayOrigin = Vector(0.25, 0.25, 2)
         rayDirection = Vector(0.1, 0, -1)
         rayDirection.normalize()
         ray = Ray(rayOrigin, rayDirection, length=2.2)
 
-        intersection = AnyIntersectStrategy().getIntersection(ray, polygon)
+        intersection = self.intersectStrategy.getIntersection(ray, self.aPolygon)
 
         self.assertIsNotNone(intersection)
         self.assertEqual(0.45, intersection.x)
         self.assertEqual(0.25, intersection.y)
         self.assertEqual(0.0, intersection.z)
+
+
+class TestMollerTrumboreTriangleIntersect(TestAnyPolygonIntersect, unittest.TestCase):
+    @property
+    def intersectStrategy(self):
+        return MollerTrumboreTriangleIntersect()
+
+    @property
+    def aPolygon(self):
+        return Triangle(self.vertices[0], self.vertices[1], self.vertices[3])
+
+
+class TestMollerTrumboreQuadIntersect(TestAnyPolygonIntersect, unittest.TestCase):
+    @property
+    def intersectStrategy(self):
+        return MollerTrumboreQuadIntersect()
+
+    @property
+    def aPolygon(self) -> Polygon:
+        return Quad(*self.vertices)
