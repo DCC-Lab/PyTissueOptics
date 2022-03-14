4b3bca7e021330ca5c170c72c2795430a496a381
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-03-14 18:08:51 -0400

    test for PahntomScene

--- pytissueoptics/scene/tests/tree/treeConstructor/binary/testBinaryNodeSplitter.py
+++ pytissueoptics/scene/tests/tree/treeConstructor/binary/testBinaryNodeSplitter.py
@@ -3,7 +3,7 @@ from mockito import mock, when
 
 from pytissueoptics.scene.geometry import Polygon, BoundingBox
 from pytissueoptics.scene.tree.treeConstructor.binary import MeanCentroidNodeSplitter, MiddlePolygonSpanNodeSplitter, \
-    HardSAHNodeSplitter, BBoxPolyCounter
+    HardSAHNodeSplitter, BBoxPolyCounter, ShrankBoxSAHNodeSplitter
 from pytissueoptics.scene.tree.treeConstructor import PolygonCounter, SplitNodeResult
 from pytissueoptics.scene.geometry import Vector
 
@@ -84,6 +84,44 @@ class TestBinaryHardSAHNodeSplitter(unittest.TestCase):
 
     def testOnXAxis_givenSplitCostOf40Percent_shouldReturnStopCondition(self):
         self.splitter = HardSAHNodeSplitter(self.polyCounter, nbOfSplitPlanes=3, splitCostPercentage=0.4)
+        splitNodeResult = self.splitter.split("x", self.nodeBbox, self.polygons)
+        validationBbox1 = self.nodeBbox = BoundingBox(xLim=[-1, 1.5], yLim=[-1, 3], zLim=[-1, 5])
+        validationBbox2 = self.nodeBbox = BoundingBox(xLim=[1.5, 4], yLim=[-1, 3], zLim=[-1, 5])
+        validationResult = SplitNodeResult(True, "x", 1.5, [validationBbox1, validationBbox2],
+                                           [[self.polygons[0], self.polygons[1]], [self.polygons[2]]])
+        self.assertEqual(validationResult, splitNodeResult)
+
+    def testOnYAxis_givenSplitCostOf20Percent_shouldReturnStopCondition(self):
+        splitNodeResult = self.splitter.split("y", self.nodeBbox, self.polygons)
+        validationBbox1 = self.nodeBbox = BoundingBox(xLim=[-1, 4], yLim=[-1, 2], zLim=[-1, 5])
+        validationBbox2 = self.nodeBbox = BoundingBox(xLim=[-1, 4], yLim=[2, 3], zLim=[-1, 5])
+        validationResult = SplitNodeResult(True, "y", 2.0, [validationBbox1, validationBbox2],
+                                           [[self.polygons[0], self.polygons[1], self.polygons[2]], [self.polygons[2]]])
+        self.assertEqual(validationResult, splitNodeResult)
+
+
+class TestBinaryShrankBoxSAHNodeSplitter(unittest.TestCase):
+    def setUp(self):
+        self.polygons = [Polygon(vertices=[Vector(0, 0, 0), Vector(0, 1, 0), Vector(1, 1, 0)]),
+                         Polygon(vertices=[Vector(0, 0, 0), Vector(0, 1, 0), Vector(-1, -2, 0)]),
+                         Polygon(vertices=[Vector(2, 2, 2), Vector(3, 3, 3), Vector(2, 3, 2)])]
+
+        self.nodeBbox = BoundingBox(xLim=[-1, 4], yLim=[-1, 3], zLim=[-1, 5])
+        self.polyCounter = BBoxPolyCounter()
+        self.splitter = ShrankBoxSAHNodeSplitter(self.polyCounter, nbOfSplitPlanes=3, splitCostPercentage=0.2,
+                                                 fallBackPercentage=0.01)
+
+    def testOnXAxis_givenSplitCostOf20Percent_shouldReturnCorrectSplitNodeResult(self):
+        splitNodeResult = self.splitter.split("x", self.nodeBbox, self.polygons)
+        validationBbox1 = self.nodeBbox = BoundingBox(xLim=[-1, 1.5], yLim=[-1, 3], zLim=[-1, 5])
+        validationBbox2 = self.nodeBbox = BoundingBox(xLim=[1.5, 4], yLim=[-1, 3], zLim=[-1, 5])
+        validationResult = SplitNodeResult(False, "x", 1.5, [validationBbox1, validationBbox2],
+                                           [[self.polygons[0], self.polygons[1]], [self.polygons[2]]])
+        self.assertEqual(validationResult, splitNodeResult)
+
+    def testOnXAxis_givenSplitCostOf40Percent_shouldReturnStopCondition(self):
+        self.splitter = ShrankBoxSAHNodeSplitter(self.polyCounter, nbOfSplitPlanes=3, splitCostPercentage=0.4,
+                                                 fallBackPercentage=0.01)
         splitNodeResult = self.splitter.split("x", self.nodeBbox, self.polygons)
         validationBbox1 = self.nodeBbox = BoundingBox(xLim=[-1, 1.5], yLim=[-1, 3], zLim=[-1, 5])
         validationBbox2 = self.nodeBbox = BoundingBox(xLim=[1.5, 4], yLim=[-1, 3], zLim=[-1, 5])
