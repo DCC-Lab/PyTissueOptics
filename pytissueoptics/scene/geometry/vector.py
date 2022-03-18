64ebf2989f9136e1dc742695054357e876fe447e
Marc-Andre Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-03-18 11:26:42 -0400

    vector: __eq__ uses math.isclose to allow for rel_tol

--- pytissueoptics/scene/geometry/vector.py
+++ pytissueoptics/scene/geometry/vector.py
@@ -1,3 +1,6 @@
+from math import isclose
+
+
 class Vector:
     """
     Basic implementation of a mutable 3D Vector. It implements most of the basic vector operation.
@@ -25,7 +28,7 @@ class Vector:
         return f"<Vector>:({self.x}, {self.y}, {self.z})"
 
     def __eq__(self, other: 'Vector'):
-        if other.x == self.x and other.y == self.y and other.z == self.z:
+        if isclose(other.x, self.x) and isclose(other.y, self.y) and isclose(other.z, self.z):
             return True
         else:
             return False
