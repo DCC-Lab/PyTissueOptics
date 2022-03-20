f6c5815d9c259745bf06516cc2121b43666a85f0
JLBegin <ludovick.begin@gmail.com>
2022-03-20 15:46:19 -0400

    Photon: make logger optional and refactor

--- pytissueoptics/rayscattering/photon.py
+++ pytissueoptics/rayscattering/photon.py
@@ -1,6 +1,5 @@
 import math
-
-import numpy as np
+import random
 
 from pytissueoptics.scene import Vector, Material
 from pytissueoptics.scene.intersection import Ray
@@ -21,20 +20,35 @@ class Photon:
 
         self._er = self._direction.anyPerpendicular()
         self._er.normalize()
+        self._hasContext = False
 
     @property
     def isAlive(self) -> bool:
         return self._weight > 0
 
-    def setContext(self, worldMaterial: Material, intersectionFinder: IntersectionFinder, logger: Logger):
+    @property
+    def position(self) -> Vector:
+        return self._position
+
+    @property
+    def direction(self) -> Vector:
+        return self._direction
+
+    def setContext(self, worldMaterial: Material, intersectionFinder: IntersectionFinder, logger: Logger = None):
         self._worldMaterial = worldMaterial
-        # todo: set proper initial material
+        # todo: set proper initial material, allow for no intersectionFinder
         self._material = worldMaterial
         self._intersectionFinder = intersectionFinder
         self._logger = logger
+        # todo: PhotonLogger with logEventX booleans:
+        #  logInitialPositions, logIntersections, logScattering, logEndPositions
+        self._hasContext = True
 
     def propagate(self):
-        self._logger.logPoint(self._position)
+        if not self._hasContext:
+            raise NotImplementedError("Cannot propagate photon without context. Use ‘setContext(...)‘. ")
+
+        self._logPosition()
         while self.isAlive:
             distance = self._material.getScatteringDistance()
             self.step(distance)
@@ -46,8 +60,10 @@ class Photon:
         intersection = self._intersectionFinder.findIntersection(stepRay)
 
         if intersection:
-            self._logger.logPoint(intersection.position)
-            self._position += self._direction * (intersection.distance + 1e-3)
+            # todo: this +1e-3 only makes sense if when refracting.
+            #  update when reflection is implemented
+            self.moveBy(intersection.distance + 1e-3)
+            self._logPosition()
 
             self.refract(intersection)
             self._updateMaterial(self._getNextMaterial(intersection))
@@ -56,8 +72,11 @@ class Photon:
             self._weight = 0
 
         else:
-            self._position += self._direction * distance
+            self.moveBy(distance)
             self.scatter()
+
+    def moveBy(self, distance):
+        self._position += self._direction * distance
 
     def refract(self, intersection):
         surfaceNormal = intersection.polygon.normal
@@ -67,10 +86,10 @@ class Photon:
             surfaceNormal *= -1
 
         incidencePlane = self._direction.cross(surfaceNormal)
-        self._direction.rotateAround(incidencePlane, self._getRefractionAngle(intersection, goingInside))
+        incidencePlane.normalize()
 
-        if self._logger:
-            self._logger.logPoint(self._position)
+        angleChange = self._getRefractionAngle(intersection, goingInside)
+        self._direction.rotateAround(incidencePlane, angleChange)
 
     def _getNextMaterial(self, intersection: Intersection):
         if intersection.polygon.normal.dot(self._direction) > 0:
@@ -105,7 +124,7 @@ class Photon:
         chance = 0.1
         if self._weight >= 1e-4 or self._weight == 0:
             return
-        elif np.random.random() < chance:
+        elif random.random() < chance:
             # todo: are we gonna talk about this?
             self._weight /= chance
         else:
@@ -130,5 +149,9 @@ class Photon:
             # We should not be here.
             raise ValueError("Can't refract beyond angle of total reflection")
 
-        thetaOut = np.arcsin(sinThetaOut)
+        thetaOut = math.asin(sinThetaOut)
         return thetaIn - thetaOut
+
+    def _logPosition(self):
+        if self._logger is not None:
+            self._logger.logPoint(self._position)
