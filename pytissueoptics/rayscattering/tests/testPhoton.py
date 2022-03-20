19388a7a81670184df25ba6dfd055435a3b51664
JLBegin <ludovick.begin@gmail.com>
2022-03-20 18:50:05 -0400

    Photon: test scatter

--- pytissueoptics/rayscattering/tests/testPhoton.py
+++ pytissueoptics/rayscattering/tests/testPhoton.py
@@ -2,6 +2,7 @@ import math
 import unittest
 
 from pytissueoptics.rayscattering import Photon
+from pytissueoptics.rayscattering.fresnelIntersect import FresnelIntersect
 from pytissueoptics.scene import Vector, Material
 from pytissueoptics.scene.geometry import Polygon
 from pytissueoptics.scene.intersection.intersectionFinder import Intersection
@@ -36,6 +37,7 @@ class TestPhoton(unittest.TestCase):
         self.assertEqual(initialPosition + self.DIRECTION * 2, self.photon.position)
 
     def testWhenRefractWithPerpendicularSurface_shouldNotRotatePhoton(self):
+        # todo: move to TestFresnelIntersect
         initialDirection = self.DIRECTION.copy()
         n1, n2 = 1, 1.4
         surfaceElement = Polygon([Vector()], normal=Vector(0, 0, 1),
@@ -43,12 +45,12 @@ class TestPhoton(unittest.TestCase):
                                  outsideMaterial=Material(index=n1))
         intersection = Intersection(1, self.POSITION + self.DIRECTION, surfaceElement)
 
-        self.photon.refract(intersection)
+        fresnelIntersection = FresnelIntersect(self.photon.direction, intersection)
+        self.photon.refract(fresnelIntersection)
 
         self.assertEqual(initialDirection, self.photon.direction)
 
     def testWhenRefract_shouldOrientPhotonTowardsFresnelRefractionAngle(self):
-        # todo: move logic to FresnelIntersect(intersection)
         incidenceAngle = math.pi / 10
         self.photon = Photon(self.POSITION, Vector(0, math.sin(incidenceAngle), -math.cos(incidenceAngle)))
         n1, n2 = 1, 1.4
@@ -57,11 +59,35 @@ class TestPhoton(unittest.TestCase):
                                  outsideMaterial=Material(index=n1))
         intersection = Intersection(1, self.POSITION + self.DIRECTION, surfaceElement)
 
-        self.photon.refract(intersection)
+        fresnelIntersection = FresnelIntersect(self.photon.direction, intersection)
+        self.photon.refract(fresnelIntersection)
 
         refractionAngle = math.asin(n1/n2 * math.sin(incidenceAngle))
         expectedDirection = Vector(0, math.sin(refractionAngle), -math.cos(refractionAngle))
         self.assertVectorEquals(expectedDirection, self.photon.direction)
+
+    def testWhenScatter_shouldDecreasePhotonWeight(self):
+        material = Material(mu_s=2, mu_a=1)
+        self.photon.setContext(material)
+
+        self.photon.scatter()
+
+        self.assertEqual(1 - material.albedo, self.photon.weight)
+
+    def testWhenScatter_shouldScatterPhotonDirection(self):
+        material = Material(mu_s=2, mu_a=1)
+        theta, phi = math.pi/5, math.pi
+        material.getScatteringAngles = lambda: (theta, phi)
+        self.photon.setContext(material)
+        self.photon._er = Vector(-1, 0, 0)
+
+        self.photon.scatter()
+
+        expectedDirection = Vector(0, math.sin(theta), -math.cos(theta))
+        self.assertVectorEquals(expectedDirection, self.photon.direction)
+
+    def testWhenReflect_shouldOrientPhotonTowardsFresnelReflectionAngle(self):
+        pass
 
     def testGivenALogger_whenPropagate_shouldLogInitialPosition(self):
         pass
