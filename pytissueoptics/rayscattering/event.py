5acfb8e470864a5289b5e4a1aab4a3364f682f20
Daniel <dccote@cervo.ulaval.ca>
2022-03-20 20:51:12 -0400

    Update event.py
    Pseudo-code for event based photon

--- pytissueoptics/rayscattering/event.py
+++ pytissueoptics/rayscattering/event.py
@@ -1,11 +1,14 @@
 from typing import NamedTuple
+from ctypes import *
 # from photon import Photon
 
 """
 Photon events
 
-MOVE, distance
-SCATTER, µs, µa
+MOVE 
+MOVEBY, distance
+SCATTER, µs, µa, g
+SCATTERBY, theta, phi
 REFLECT normal, n1, n2 
 REFRACT normal, n1, n2
 
@@ -25,30 +28,34 @@ class Vector:
 
 class Name(IntEnum):
     MOVE = 1
+    MOVEBY = 9
     SCATTER = 2
+    SCATTERBY = 10
     AT_INTERFACE = 3
     REFLECT = 4
     REFRACT = 5
     ROULETTE = 6
+    STORE = 11
+
+class CompactMaterial(NamedTuple):
+    mu_s:float = 0
+    mu_a:float = 0
+    g:float = 0
+    n1:float = 0
+    n2:float = 0
 
 class Event(NamedTuple):
     which: Name
     processing : None
     distance: float = None
-    n1: float = None
-    n2: float = None
-    normal: Vector = None
-    material1 = None
-    material2 = None
+    theta: float = None
+    phi: float = None
+    intersection:Intersection = None
+    materialInside:CompactMaterial = None
+    materialOutside:CompactMaterial = None
 
 
 class EventDrivenPhoton:
-    # MOVE = Event(Name.MOVE, EventDrivenPhoton.processMoveEvent)
-    # SCATTER = Event(Name.SCATTER, EventDrivenPhoton.processScatterEvent)
-    # AT_INTERFACE = Event(Name.AT_INTERFACE, EventDrivenPhoton.processAtInterfaceEvent)
-    # REFLECT = Event(Name.REFLECT, EventDrivenPhoton.processReflectEvent)
-    # REFRACT = Event(Name.REFRACT, EventDrivenPhoton.processRefractEvent)
-
     def __init__(self, position, direction):
         self.nextEvent = None
         super().__init__(position, direction)
@@ -58,7 +65,11 @@ class EventDrivenPhoton:
             self.nextEvent.processing()
 
     def processMoveEvent(self):
-        intersection = self.getIntersection(event.distance)
+        d = self.getScatteringDistance(mu_s, mu_a, g)
+        self.nextEvent = Event(MOVEBY, self.processMoveByEvent, distance=d)
+
+    def processMoveByEvent(self):
+        intersection = self.getIntersection(d)
         if intersection is None:
             self.move(event.distance)
             self.nextEvent = Event(SCATTER, self.processScatterEvent)
@@ -76,19 +87,22 @@ class EventDrivenPhoton:
 
     def processScatterEvent(self):
         theta, phi = self.getScatteringAngles(mu_s, mu_a, g)
-        self.scatter(theta, phi)
-        d = self.getScatteringDistance(mu_s, mu_a, g)
+        self.nextEvent = Event(SCATTERBY, self.processRouletteEvent)
+
+    def processScatterByEvent(self):
+        self.scatter(self.nextEvent.theta, self.nextEvent.phi)
+        self.decreaseWeight(0.1)
         self.nextEvent = Event(ROULETTE, self.processRouletteEvent)
 
     def processReflectEvent(self):
         interfaceEvent = self.nextEvent
         self.reflect(interfaceEvent.normal, interfaceEvent.n1, interfaceEvent.n2)
-        self.nextEvent = Event(MOVE, self.processMoveEvent)
+        self.nextEvent = Event(MOVEBY, self.processMoveEvent, remainingDistance)
 
     def processRefractEvent(self):
         interfaceEvent = self.nextEvent
         self.refract(interfaceEvent.normal, interfaceEvent.n1, interfaceEvent.n2)
-        self.nextEvent = Event(MOVE, self.processMoveEvent)
+        self.nextEvent = Event(MOVEBY, self.processMoveEvent, remainingDistance)
 
     def processRouletteEvent(self):
         isDead =  self.roulette()
