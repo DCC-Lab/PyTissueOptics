3f201b2c1be1ee1f8f2bad5c068403d9304644e8
JLBegin <ludovick.begin@gmail.com>
2022-03-21 12:34:44 -0400

    Improve test coverage a bit

--- pytissueoptics/scene/tests/geometry/testPolygon.py
+++ pytissueoptics/scene/tests/geometry/testPolygon.py
@@ -12,7 +12,7 @@ class TestPolygon(unittest.TestCase):
         polygon = Polygon(vertices=[Vector(0, 0, 1), Vector(2, 0, 0), Vector(2, 2, 0), Vector(1, 1, 0)])
         self.assertEqual(Vector(5/4, 3/4, 1/4), polygon.centroid)
 
-    def testGivenANewPolygon_whenModifyingVertex_resetBoundingBoxShouldChangeBbox(self):
+    def testGivenANewPolygon_whenModifyingVertexAndResetBoundingBox_shouldChangeBbox(self):
         triangle = Polygon(vertices=[Vector(0, 0, 0), Vector(2, 0, 0), Vector(2, 2, 0), Vector(1, 4, 2)])
         oldBbox = triangle.bbox
         triangle.vertices[0].update(5, 1, 1)
@@ -29,3 +29,7 @@ class TestPolygon(unittest.TestCase):
         polygon1 = Polygon(vertices=[Vector(0, 0, 0), Vector(2, 0, 0), Vector(2, 2, 0), Vector(1, 1, 0)])
         polygon2 = Polygon(vertices=[Vector(1, 0, 0), Vector(2, 2, 0), Vector(1, 1, 0), Vector(0, 0, 0)])
         self.assertNotEqual(polygon1, polygon2)
+
+    def testWhenGetCentroid_shouldReturnAverageVertex(self):
+        polygon = Polygon(vertices=[Vector(0, 0, 0), Vector(2, 0, 0), Vector(2, 2, 0), Vector(0, 2, 0)])
+        self.assertEqual(Vector(1, 1, 0), polygon.getCentroid())
