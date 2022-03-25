464cd5c2131a3d4d955f06f71bd080a69707627f
JLBegin <ludovick.begin@gmail.com>
2022-03-25 12:09:55 -0400

    Update example.py

--- pytissueoptics/rayscattering/example.py
+++ pytissueoptics/rayscattering/example.py
@@ -1,19 +1,17 @@
 from pytissueoptics.rayscattering import *
 from pytissueoptics.rayscattering.tissues import PhantomTissue
-from pytissueoptics.scene import *
+from pytissueoptics.scene import Vector, Logger, MayaviViewer
 import numpy as np
 
 np.random.seed(15)
 
-viewer = MayaviViewer()
 tissue = PhantomTissue()
-tissue.addToViewer(viewer)
-
-source = PencilSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1),
-                      N=2000)
-
+source = PencilSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1), N=2000)
 logger = Logger()
+
 source.propagate(tissue, logger=logger)
 
+viewer = MayaviViewer()
+tissue.addToViewer(viewer)
 viewer.addLogger(logger)
 viewer.show()
