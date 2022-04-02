e4240eb417784515b07fd2b76e740ca9ea089bdf
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-04-02 14:06:12 -0400

    problem: kernels change weight on whatever object they want

--- pytissueoptics/rayscattering/opencl/warmup.c
+++ pytissueoptics/rayscattering/opencl/warmup.c
@@ -11,9 +11,34 @@
 // output: partial_sums   float vector of partial sums
 //
 
-
-__kernel void moveByOne(__global photonStruct *a)
+__kernel void moveBy(__global photonStruct *a, float step_size)
 {
-    int ix = get_global_id(0);
-    a[ix].position += a[ix].direction;
+    int gid = get_global_id(0);
+        a[gid].position += step_size * a[gid].direction;
+
 }
+
+__kernel void decreaseWeightBy(__global photonStruct *a, float delta_weight)
+{
+    int gid = get_global_id(0);
+        a[gid].weight -= delta_weight;
+}
+
+__kernel void propagate(__global photonStruct *a)
+{
+    int gid = get_global_id(0);
+    while (a[gid].weight > 0.0f)
+    {
+        moveBy(a, 1.0f);
+        decreaseWeightBy(a, 0.01f);
+    }
+
+}
+//__kernel void roulette(__global photonStruct *a, __global float *rdn)
+//{
+//    int gid = get_global_id(0);
+//    float chance = 0.1;
+//    if (a[gid].weight >= 1e-4 || a[gid].weight == 0){return;}
+//    else if (rnd[gid] < chance){a[gid].weight /= chance;}
+//    else{a[gid].weight = 0;}
+//}
--- pytissueoptics/rayscattering/opencl/warmup.py
+++ pytissueoptics/rayscattering/opencl/warmup.py
@@ -1,6 +1,6 @@
 from pytissueoptics import *
 import pyopencl as cl
-from pyopencl.elementwise import ElementwiseKernel
+import pyopencl.cltypes
 import pyopencl.array
 import pyopencl.tools
 import numpy as np
@@ -13,7 +13,10 @@ device = context.devices[0]
 
 def makePhotonType(device):
     myStruct = np.dtype(
-        [("position", cl.array.vec.float4), ("direction", cl.array.vec.float4)])
+        [("position", cl.cltypes.float4),
+         ("direction", cl.cltypes.float4),
+         ("er", cl.cltypes.float4),
+         ("weight", cl.cltypes.float)])
     name = "photonStruct"
     dtype, c_decl = cl.tools.match_dtype_to_c_struct(device, name, myStruct)
     dtype = cl.tools.get_or_register_dtype(name, myStruct)
@@ -25,15 +28,19 @@ print(photon_c_decl)
 N = 10000
 
 HOST_photons = np.empty(N, dtype=photon_dtype)
-HOST_photons["position"] = np.array([cl.array.vec.make_float4(1, 0, 0, 0)]*N, dtype=cl.array.vec.float4)
-HOST_photons["direction"] = np.array([cl.array.vec.make_float4(0, 0, 1, 0)]*N, dtype=cl.array.vec.float4)
-
-
-print(HOST_photons)
-
+HOST_photons["position"] = np.array([cl.cltypes.make_float4(1, 0, 0, 0)]*N, dtype=cl.cltypes.float4)
+HOST_photons["direction"] = np.array([cl.cltypes.make_float4(0, 0, 1, 0)]*N, dtype=cl.cltypes.float4)
+HOST_photons["er"] = np.array([cl.cltypes.make_float4(0, 1, 0, 0)]*N, dtype=cl.cltypes.float4)
+HOST_photons["weight"] = np.ones(N, dtype=cl.cltypes.float)
 TARGET_photons = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=HOST_photons)
 cl.enqueue_copy(queue, dest=TARGET_photons, src=HOST_photons)
-program.moveByOne(queue, (N,), None, TARGET_photons)
+
+#HOST_randRoulette = np.random.rand(N)
+#TARGET_randRoulette = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=HOST_randRoulette)
+#cl.enqueue_copy(queue, dest=TARGET_randRoulette, src=HOST_randRoulette)
+
+
+program.propagate(queue, (N,), None, TARGET_photons)
 cl.enqueue_copy(queue, dest=HOST_photons, src=TARGET_photons)
 queue.finish()
 
