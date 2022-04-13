4cbf4d5277ad424e976dfaf0a1a1679fda186b1e
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-04-13 00:30:58 -0400

    interact, rotateAround, scatterBy, getScatteringAngles

--- pytissueoptics/rayscattering/opencl/CLPhotons.c
+++ pytissueoptics/rayscattering/opencl/CLPhotons.c
@@ -49,6 +49,12 @@ void decreaseWeightBy(__global photonStruct *photons, float delta_weight, uint g
         photons[gid].weight -= delta_weight;
 }
 
+void interact(__global photonStruct *photons, __constant materialStruct *materials, uint gid)
+{
+    float delta_weight = photons[gid].weight * materials[photons[gid].material_id].albedo;
+    decreaseWeightBy(photons, delta_weight, gid);
+}
+
 void moveBy(__global photonStruct *photons, float distance, uint gid)
 {
         photons[gid].position += distance * photons[gid].direction;
@@ -59,16 +65,66 @@ float getScatteringDistance(__global photonStruct *photons,__constant materialSt
     return -log(randomNums[gid]) / materials[photons[gid].material_id].albedo;
 }
 
+float getScatteringAnglePhi(__global photonStruct *photons, __global float * randomNums, uint gid)
+{
+    float phi = 2.0f * M_PI * randomNums[gid];
+    return phi;
+}
 
+float getScatteringAngleTheta(__global photonStruct *photons,__constant materialStruct *materials,  __global float * randomNums, uint gid)
+{
+    if (materials[gid].g == 0){
+        return acos(2.0f * randomNums[gid] - 1.0f);
+    }
+    else{
+        float temp = (1.0f - materials[gid].g * materials[gid].g) / (1 - materials[gid].g + 2 * materials[gid].g * randomNums[gid]);
+        return acos((1.0f + materials[gid].g * materials[gid].g - temp * temp) / (2 * materials[gid].g));
+    }
+}
+
+float4 rotateAround(float4 mainVector, float4 axisVector, float theta)
+{
+    float sint = sin(theta);
+    float cost = cos(theta);
+    float one_cost = 1.0f - cost;
+    float ux = axisVector.x;
+    float uy = axisVector.y;
+    float uz = axisVector.z;
+    float X = mainVector.x;
+    float Y = mainVector.y;
+    float Z = mainVector.z;
+    float x = (cost + ux * ux * one_cost) * X \
+            + (ux * uy * one_cost - uz * sint) * Y \
+            + (ux * uz * one_cost + uy * sint) * Z;
+    float y = (uy * ux * one_cost + uz * sint) * X \
+            + (cost + uy * uy * one_cost) * Y \
+            + (uy * uz * one_cost - ux * sint) * Z;
+    float z = (uz * ux * one_cost - uy * sint) * X \
+            + (uz * uy * one_cost + ux * sint) * Y \
+            + (cost + uz * uz * one_cost) * Z;
+    return (float4)(x, y, z, 0.0f);
+}
+
+void scatterBy(__global photonStruct *photons, float phi, float theta, uint gid)
+{
+    photons[gid].er = rotateAround(photons[gid].er, photons[gid].direction, phi);
+    photons[gid].direction = rotateAround(photons[gid].direction, photons[gid].er, theta);
+}
 // PROPAGATE KERNELS
 __kernel void propagate(__global photonStruct *photons, __constant materialStruct *materials, __global float *randomNums, __global uint * rnd_buffer)
 {
     int gid = get_global_id(0);
-    while(photons[gid].weight > 0.0001)
-    {
+    //while(photons[gid].weight > 0.0001)
+    //{
+    for (int i = 0; i < 5; i++){
         randomNums[gid] = random_float(rnd_buffer, gid);
         float distance = getScatteringDistance(photons, materials, randomNums, gid);
         moveBy(photons, distance, gid);
-        decreaseWeightBy(photons, 0.3f, gid);
+        randomNums[gid] = random_float(rnd_buffer, gid);
+        float phi = getScatteringAnglePhi(photons, randomNums, gid);
+        randomNums[gid] = random_float(rnd_buffer, gid);
+        float theta = getScatteringAngleTheta(photons, materials, randomNums, gid);
+        scatterBy(photons, phi, theta, gid);
+        interact(photons, materials, gid);
     }
 }
