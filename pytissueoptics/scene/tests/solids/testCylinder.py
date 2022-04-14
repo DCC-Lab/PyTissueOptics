f284e1303bcae44bcc7ce9dcf0fd764ba0541a71
JLBegin <ludovick.begin@gmail.com>
2022-04-14 19:34:11 -0400

    Cylinder: use Vertex

--- pytissueoptics/scene/tests/solids/testCylinder.py
+++ pytissueoptics/scene/tests/solids/testCylinder.py
@@ -1,7 +1,7 @@
 import unittest
 import math
 
-from pytissueoptics.scene.geometry import Vector
+from pytissueoptics.scene.geometry import Vector, Vertex
 from pytissueoptics.scene.solids import Cylinder
 
 
@@ -43,23 +43,23 @@ class TestCylinder(unittest.TestCase):
     def testWhenContainsWithVerticesThatAreAllInsideTheCylinder_shouldReturnTrue(self):
         cylinder = Cylinder(radius=1, height=3, u=32, v=2, position=Vector(2, 2, 0))
         cylinder.rotate(0, 30, 0)
-        vertices = [Vector(3.4, 2, 2.4), Vector(2, 2, 0.5)]
+        vertices = [Vertex(3.4, 2, 2.4), Vertex(2, 2, 0.5)]
 
         self.assertTrue(cylinder.contains(*vertices))
 
     def testWhenContainsWithVerticesThatAreNotAllInsideTheCylinder_shouldReturnFalse(self):
         cylinder = Cylinder(radius=1, height=3, u=32, v=2, position=Vector(2, 2, 0))
         cylinder.rotate(0, 30, 0)
-        vertices = [Vector(3.51, 2, 2.6), Vector(2, 2, 0.5)]
+        vertices = [Vertex(3.51, 2, 2.6), Vertex(2, 2, 0.5)]
                 
         self.assertFalse(cylinder.contains(*vertices))
 
     def testWhenContainsWithVerticesOutsideMinRadius_shouldReturnFalse(self):
         cylinder = Cylinder(radius=1000, height=3, u=6, position=Vector(0, 0, 0))
-        vertices = [Vector(0, 867, 0)]
+        vertices = [Vertex(0, 867, 0)]
         self.assertFalse(cylinder.contains(*vertices))
 
     def testWhenContainsWithVerticesInsideMinRadius_shouldReturnTrue(self):
         cylinder = Cylinder(radius=1000, height=3, u=6, position=Vector(0, 0, 0))
-        vertices = [Vector(0, 866, 0)]
+        vertices = [Vertex(0, 866, 0)]
         self.assertTrue(cylinder.contains(*vertices))
