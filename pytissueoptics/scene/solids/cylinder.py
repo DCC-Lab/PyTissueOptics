c722801500b5f4c153a5e0cb99904192997bd8e5
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-04-14 17:29:33 -0400

    typehint + checkstyle

--- pytissueoptics/scene/solids/cylinder.py
+++ pytissueoptics/scene/solids/cylinder.py
@@ -1,9 +1,7 @@
 from math import cos, sin, pi
-from typing import Optional
+from typing import List
 
-from pytissueoptics import Segment
-from pytissueoptics.scene.viewer import MayaviViewer
-from pytissueoptics.scene.geometry import Vector, Triangle, primitives, utils
+from pytissueoptics.scene.geometry import Vector, Triangle, primitives
 from pytissueoptics.scene.materials import Material
 from pytissueoptics.scene.solids import Solid
 
@@ -21,7 +19,8 @@ class Cylinder(Solid):
         self._bottomCenter = Vector(0, 0, 0)
         self._topCenter = Vector(0, 0, height)
         self._minRadius = cos(pi / self._u) * self._radius
-        super().__init__(position=position, material=material, primitive=primitive, vertices=[self._bottomCenter, self._topCenter])
+        super().__init__(position=position, material=material, primitive=primitive,
+                         vertices=[self._bottomCenter, self._topCenter])
 
     @property
     def direction(self) -> Vector:
@@ -33,7 +32,7 @@ class Cylinder(Solid):
         self._computeBottomTriangles(verticesGroups[0])
         self._computeTopTriangles(verticesGroups[-1])
 
-    def _computeVertices(self):
+    def _computeVertices(self) -> List[List[Vector]]:
         verticesGroups = []
         radianStep = 2 * pi / self._u
         verticalStep = self._height / self._v
@@ -55,7 +54,7 @@ class Cylinder(Solid):
         z = j * verticalStep
         return Vector(x, y, z)
 
-    def _computeMiddleTriangles(self, verticesGroups):
+    def _computeMiddleTriangles(self, verticesGroups: List[List[Vector]]):
         middleTriangles = []
         for i in range(self._v):
             currentGroup = verticesGroups[i]
@@ -66,14 +65,14 @@ class Cylinder(Solid):
                 middleTriangles.append(Triangle(currentGroup[j], currentGroup[nextIndex], nextGroup[nextIndex]))
         self._surfaces.add("Middle", middleTriangles)
 
-    def _computeBottomTriangles(self, vertices):
+    def _computeBottomTriangles(self, vertices: List[Vector]):
         bottomTriangles = []
         for i in range(self._u):
             nextIndex = (i + 1) % self._u
             bottomTriangles.append(Triangle(self._bottomCenter, vertices[i], vertices[nextIndex]))
         self._surfaces.add("Bottom", bottomTriangles)
 
-    def _computeTopTriangles(self, vertices):
+    def _computeTopTriangles(self, vertices: List[Vector]):
         topTriangles = []
         for i in range(self._u):
             nextIndex = (i + 1) % self._u
