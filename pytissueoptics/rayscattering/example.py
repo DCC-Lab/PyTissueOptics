1ff582728e6531b50d9259200e333d4f11a6dcd6
JLBegin <ludovick.begin@gmail.com>
2022-04-21 15:03:06 -0400

    create Stats: 3D display of energy

--- pytissueoptics/rayscattering/example.py
+++ pytissueoptics/rayscattering/example.py
@@ -1,17 +1,17 @@
 from pytissueoptics.rayscattering import *
+from pytissueoptics.rayscattering import Stats
 from pytissueoptics.rayscattering.tissues import PhantomTissue
-from pytissueoptics.scene import Vector, Logger, MayaviViewer
+from pytissueoptics.scene import Vector, Logger
 import numpy as np
 
 np.random.seed(15)
 
 tissue = PhantomTissue()
-source = PencilSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1), N=2000)
+source = PencilSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1), N=1000)
 logger = Logger()
 
 source.propagate(tissue, logger=logger)
 
-viewer = MayaviViewer()
-tissue.addToViewer(viewer)
-viewer.addLogger(logger)
-viewer.show()
+stats = Stats(logger, tissue)
+stats.showEnergy3D()
+stats.showEnergy3DOfSurfaces()
---
+++ pytissueoptics/rayscattering/stats.py
@@ -0,0 +1,92 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+from pytissueoptics.scene import Logger, Vector, Scene
+from pytissueoptics.scene.logger import DataPoint, InteractionKey
+
+
+@dataclass
+class PointCloud:
+    solidPoints: Optional[List[DataPoint]] = None
+    surfacePoints: Optional[List[DataPoint]] = None
+
+
+class DisplayConfig:
+    """ 3D display configuration dataclass for solid and surface point cloud. """
+    def __init__(self, showObject: bool = True,
+                 pointSize: float = 0.15, scaleWithValue: bool = True, colormap: str = "rainbow", reverseColormap: bool = False,
+                 surfacePointSize: float = 0.01, surfaceScaleWithValue: bool = False, surfaceColormap: str = None, surfaceReverseColormap: bool = None):
+        self.showObject = showObject  # todo
+
+        self.pointSize = pointSize
+        self.scaleWithValue = scaleWithValue
+        self.colormap = colormap
+        self.reverseColormap = reverseColormap
+
+        self.surfacePointSize = surfacePointSize
+        self.surfaceScaleWithValue = surfaceScaleWithValue
+        self.surfaceColormap = colormap if surfaceColormap is None else surfaceColormap
+        self.surfaceReverseColormap = reverseColormap if surfaceReverseColormap is None else surfaceReverseColormap
+
+
+class Stats:
+    def __init__(self, logger: Logger, scene: Scene):
+        self._logger = logger
+        self._scene = scene
+
+    def showEnergy3D(self, solidLabel: str = None, surfaceLabel: str = None, config=DisplayConfig()):
+        pointCloud = self.getPointCloud(solidLabel, surfaceLabel)
+        return self._show3DPointCloud(pointCloud, config=config)
+
+    def showEnergy3DOfSolids(self, config=DisplayConfig()):
+        pointCloud = self._getPointCloudOfSolids()
+        return self._show3DPointCloud(pointCloud, config=config)
+
+    def showEnergy3DOfSurfaces(self, solidLabel: str = None, config=DisplayConfig()):
+        pointCloud = self._getPointCloudOfSurfaces(solidLabel)
+        return self._show3DPointCloud(pointCloud, config=config)
+
+    @staticmethod
+    def _show3DPointCloud(pointCloud: PointCloud, config: DisplayConfig):
+        from pytissueoptics.scene import MayaviViewer
+        viewer = MayaviViewer()
+        if pointCloud.solidPoints:
+            viewer.addDataPoints(pointCloud.solidPoints, scale=config.pointSize,
+                                 scaleWithValue=config.scaleWithValue, colormap=config.colormap,
+                                 reverseColormap=config.reverseColormap)
+        if pointCloud.surfacePoints:
+            viewer.addDataPoints(pointCloud.surfacePoints, scale=config.surfacePointSize,
+                                 scaleWithValue=config.surfaceScaleWithValue, colormap=config.surfaceColormap,
+                                 reverseColormap=config.surfaceReverseColormap)
+        viewer.show()
+
+    def getPointCloud(self, solidLabel: str = None, surfaceLabel: str = None) -> PointCloud:
+        if not solidLabel and not surfaceLabel:
+            return PointCloud(self._getPointCloudOfSolids().solidPoints,
+                              self._getPointCloudOfSurfaces().surfacePoints)
+        points = self._logger.getDataPoints(InteractionKey(solidLabel, surfaceLabel))
+        if surfaceLabel:
+            return PointCloud(None, points)
+        return PointCloud(points, None)
+
+    def _getPointCloudOfSolids(self) -> PointCloud:
+        points = []
+        for solidLabel in self._logger.getSolidLabels():
+            points.extend(self.getPointCloud(solidLabel).solidPoints)
+        return PointCloud(points, None)
+
+    def _getPointCloudOfSurfaces(self, solidLabel: str = None) -> PointCloud:
+        points = []
+        solidLabels = [solidLabel] if solidLabel else [_solidLabel for _solidLabel in self._logger.getSolidLabels()]
+        for _solidLabel in solidLabels:
+            for surfaceLabel in self._logger.getSurfaceLabels(_solidLabel):
+                points.extend(self.getPointCloud(_solidLabel, surfaceLabel).surfacePoints)
+        return PointCloud(None, points)
+
+    def showEnergy2D(self, solidLabel: str, surfaceLabel: str = None, direction: Vector = None):
+        raise NotImplementedError()
+
+
+# todo: test logger.getDataPoints(Key[None, None]), and test keyError
+# todo: accept Scene and display related polygons in 3D display
+# todo: rename surfaceLabels to lowercase
