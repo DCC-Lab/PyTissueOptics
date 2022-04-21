de155593204201d7c5618d4b18b2403c51104a13
JLBegin <ludovick.begin@gmail.com>
2022-04-21 15:47:03 -0400

    update tree tests to use Vertex

--- pytissueoptics/scene/tests/tree/treeConstructor/binary/testNoSplitConstructor.py
+++ pytissueoptics/scene/tests/tree/treeConstructor/binary/testNoSplitConstructor.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pytissueoptics.scene.geometry import Triangle, Vector
+from pytissueoptics.scene.geometry import Triangle, Vertex
 from pytissueoptics.scene.tree import Node
 from pytissueoptics.scene.tree.treeConstructor.binary import NoSplitOneAxisConstructor
 
@@ -10,9 +10,9 @@ class TestModernKDTreeConstructor(unittest.TestCase):
         self._fbtc = NoSplitOneAxisConstructor()
 
     def testGivenPolygons_whenClassifying_shouldReturnCorrect3Groups(self):
-        triangle0 = Triangle(Vector(0, 0, 0), Vector(1, 0, 0), Vector(1, 1, 0))
-        triangle1 = Triangle(Vector(-5, -5, 0), Vector(-5, 5, 0), Vector(5, 5, 0))
-        triangle2 = Triangle(Vector(3, -3, 2), Vector(1, -1, 1), Vector(1, -1, 1))
+        triangle0 = Triangle(Vertex(0, 0, 0), Vertex(1, 0, 0), Vertex(1, 1, 0))
+        triangle1 = Triangle(Vertex(-5, -5, 0), Vertex(-5, 5, 0), Vertex(5, 5, 0))
+        triangle2 = Triangle(Vertex(3, -3, 2), Vertex(1, -1, 1), Vertex(1, -1, 1))
 
         toClassify = [triangle1, triangle2, triangle0]
         self._fbtc.currentNode = Node(polygons=toClassify)
@@ -26,7 +26,7 @@ class TestModernKDTreeConstructor(unittest.TestCase):
 
     def testGivenTrianglesWith1ContainedVertex_whenClassifying_shouldSendTriangleOnGoodSide(self):
         with self.subTest("left"):
-            triangle0 = Triangle(Vector(0, 0, 0), Vector(1, 0, 0), Vector(1, 1, 0))
+            triangle0 = Triangle(Vertex(0, 0, 0), Vertex(1, 0, 0), Vertex(1, 1, 0))
             toClassify = [triangle0]
             self._fbtc.currentNode = Node(polygons=toClassify)
             left, right, both = self._fbtc._classifyNodePolygons("y", 1)
@@ -34,7 +34,7 @@ class TestModernKDTreeConstructor(unittest.TestCase):
             self.assertListEqual(right, [])
             self.assertListEqual(both, [])
         with self.subTest("right"):
-            triangle0 = Triangle(Vector(0, 2, 0), Vector(1, 2, 0), Vector(1, 1, 0))
+            triangle0 = Triangle(Vertex(0, 2, 0), Vertex(1, 2, 0), Vertex(1, 1, 0))
             toClassify = [triangle0]
             self._fbtc.currentNode = Node(polygons=toClassify)
             left, right, both = self._fbtc._classifyNodePolygons("y", 1)
@@ -42,7 +42,7 @@ class TestModernKDTreeConstructor(unittest.TestCase):
             self.assertListEqual(right, [triangle0])
             self.assertListEqual(both, [])
         with self.subTest("toSplit"):
-            triangle0 = Triangle(Vector(0, 2, 0), Vector(1, 0, 0), Vector(1, 1, 0))
+            triangle0 = Triangle(Vertex(0, 2, 0), Vertex(1, 0, 0), Vertex(1, 1, 0))
             toClassify = [triangle0]
             self._fbtc.currentNode = Node(polygons=toClassify)
             left, right, toSplit = self._fbtc._classifyNodePolygons("y", 1)
@@ -52,7 +52,7 @@ class TestModernKDTreeConstructor(unittest.TestCase):
 
     def testGivenTrianglesWith2ContainedVertex_whenClassifying_shouldSendTriangleOnGoodSide(self):
         with self.subTest("left"):
-            triangle0 = Triangle(Vector(0, 0, 0), Vector(1, 1, 0), Vector(1, 1, 0))
+            triangle0 = Triangle(Vertex(0, 0, 0), Vertex(1, 1, 0), Vertex(1, 1, 0))
             toClassify = [triangle0]
             self._fbtc.currentNode = Node(polygons=toClassify)
             left, right, both = self._fbtc._classifyNodePolygons("y", 1)
@@ -60,7 +60,7 @@ class TestModernKDTreeConstructor(unittest.TestCase):
             self.assertListEqual(right, [])
             self.assertListEqual(both, [])
         with self.subTest("right"):
-            triangle0 = Triangle(Vector(0, 2, 0), Vector(1, 1, 0), Vector(1, 1, 0))
+            triangle0 = Triangle(Vertex(0, 2, 0), Vertex(1, 1, 0), Vertex(1, 1, 0))
             toClassify = [triangle0]
             self._fbtc.currentNode = Node(polygons=toClassify)
             left, right, both = self._fbtc._classifyNodePolygons("y", 1)
@@ -69,8 +69,8 @@ class TestModernKDTreeConstructor(unittest.TestCase):
             self.assertListEqual(both, [])
 
     def testGivenTrianglesWith3ContainedVertex_whenClassifying_shouldSendLeftAndToSplit(self):
-        triangle00 = Triangle(Vector(0, 1, 0), Vector(1, 0, 0), Vector(1, 1, 0))
-        triangle0 = Triangle(Vector(0, 1, 0), Vector(1, 1, 0), Vector(1, 1, 0))
+        triangle00 = Triangle(Vertex(0, 1, 0), Vertex(1, 0, 0), Vertex(1, 1, 0))
+        triangle0 = Triangle(Vertex(0, 1, 0), Vertex(1, 1, 0), Vertex(1, 1, 0))
         toClassify = [triangle00, triangle0]
         self._fbtc.currentNode = Node(polygons=toClassify)
         left, right, toSplit = self._fbtc._classifyNodePolygons("y", 1)
