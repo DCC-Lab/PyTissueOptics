01cae9b07071bd93b1cbfa0c5f856cda221a3520
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-05-10 09:16:20 -0400

    rename ex05.py for future merge conflict

--- examples/ex05.py
+++ examples/ex05.py
@@ -10,13 +10,13 @@ give objects a material before you can propagate photons in them.
 
 
 def exampleCode():
-    material = ScatteringMaterial(mu_s=1.0, mu_a=1.0, g=0.9)
+    material = ScatteringMaterial(mu_s=5.0, mu_a=1.0, g=0.9)
 
     toyModel = SolidFromFile("exampleFile.obj", position=Vector(0, 0, 0), material=material)
     myCustomScene = RayScatteringScene([toyModel])
 
     logger = Logger()
-    source = IsotropicPointSource(position=Vector(-1, 4, 0), N=10000)
+    source = IsotropicPointSource(position=Vector(-1, 4, 0), N=1000)
     source.propagate(myCustomScene, logger)
 
     stats = Stats(logger, source, myCustomScene)
