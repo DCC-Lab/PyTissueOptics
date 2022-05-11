1f927df2fbf87c20fa74b0793bd5859b8962d598
JLBegin <ludovick.begin@gmail.com>
2022-05-11 12:37:43 -0400

    test checkstyle

--- pytissueoptics/scene/tests/logger/testListArrayContainer.py
+++ pytissueoptics/scene/tests/logger/testListArrayContainer.py
@@ -10,71 +10,80 @@ class TestListArrayContainer(unittest.TestCase):
         self.listArrayContainer = ListArrayContainer()
         self.otherListArrayContainer = ListArrayContainer()
 
-    def test_whenCreatingListArrayContainer_lengthShouldBeZero(self):
+    def testShouldHaveLengthOfZero(self):
         self.assertEqual(0, len(self.listArrayContainer))
 
-    def test_whenNewListArrayContainer_AttributesShouldBeNone(self):
+    def testShouldInitializeDataToNone(self):
         self.assertIsNone(self.listArrayContainer.list)
         self.assertIsNone(self.listArrayContainer.array)
         self.assertIsNone(self.listArrayContainer.mergedData)
 
-    def test_whenAppendingList_listShouldBeSet(self):
+    def testWhenAppendingList_shouldSetList(self):
         self.listArrayContainer.append([1, 2, 3])
         self.assertEqual([[1, 2, 3]], self.listArrayContainer.list)
 
-    def test_whenAppendingArray_arrayShouldBeSet(self):
+    def testWhenAppendingArray_shouldSetArray(self):
         self.listArrayContainer.append(np.array([[1, 2, 3]]))
         self.assertTrue(np.array_equal(np.array([[1, 2, 3]]), self.listArrayContainer.array))
 
-    def test_whenMergingData_withListNone_shouldSetToArray(self):
+    def testWhenMergingArrayData_shouldCreateMergedDataAsArrayType(self):
         self.listArrayContainer.append(np.array([[1, 2, 3]]))
         self.listArrayContainer.merge()
         self.assertTrue(np.array_equal(np.array([[1, 2, 3]]), self.listArrayContainer.mergedData))
 
-    def test_whenMergingData_withArrayNone_shouldSetToList(self):
+    def testWhenMergingListData_shouldCreateMergedDataAsArrayType(self):
         self.listArrayContainer.append([1, 2, 3])
         self.listArrayContainer.merge()
         self.assertTrue(np.array_equal(np.array([[1, 2, 3]]), self.listArrayContainer.mergedData))
 
-    def test_whenMergingData_withBothSet_shouldSetToAnArrayWithBothData(self):
+    def testWhenMergingData_shouldCreateMergedDataAsArrayType(self):
         self.listArrayContainer.append([1, 2, 3])
         self.listArrayContainer.append(np.array([[4, 5, 6]]))
+
         self.listArrayContainer.merge()
+
         self.assertTrue(np.array_equal(np.array([[1, 2, 3], [4, 5, 6]]), self.listArrayContainer.mergedData))
 
-    def test_whenExtendingList_listShouldBeExtendedOnFirstObject(self):
+    def testWhenExtendingWithListData_shouldOnlyExtendThisContainer(self):
         self.listArrayContainer.append([1, 2, 3])
         self.otherListArrayContainer.append([4, 5, 6])
+
         self.listArrayContainer.extend(self.otherListArrayContainer)
+
         self.assertEqual([[1, 2, 3], [4, 5, 6]], self.listArrayContainer.list)
         self.assertEqual([[4, 5, 6]], self.otherListArrayContainer.list)
         self.assertIsNone(self.otherListArrayContainer.array)
 
-    def test_whenExtendingArray_arrayShouldBeExtendedOnFirstObject(self):
+    def testWhenExtendingWithArrayData_shouldOnlyExtendThisContainer(self):
         self.listArrayContainer.append(np.array([[1, 2, 3]]))
         self.otherListArrayContainer.append(np.array([[4, 5, 6]]))
+
         self.listArrayContainer.extend(self.otherListArrayContainer)
+
         self.assertTrue(np.array_equal(np.array([[1, 2, 3], [4, 5, 6]]), self.listArrayContainer.array))
         self.assertTrue(np.array_equal(np.array([[4, 5, 6]]), self.otherListArrayContainer.array))
         self.assertIsNone(self.otherListArrayContainer.list)
 
-    def test_whenExtendingBoth_listShouldBeExtendedOnFirstObject(self):
+    def testWhenExtending_shouldOnlyExtendThisContainer(self):
         self.listArrayContainer.append([1, 2, 3])
         self.listArrayContainer.append(np.array([[4, 5, 6]]))
         self.otherListArrayContainer.append([7, 8, 9])
         self.otherListArrayContainer.append(np.array([[10, 11, 12]]))
+
         self.listArrayContainer.extend(self.otherListArrayContainer)
         self.listArrayContainer.merge()
+
         self.assertTrue(np.array_equal(np.array([[1, 2, 3], [7, 8, 9], [4, 5, 6], [10, 11, 12]]),
                                        self.listArrayContainer.mergedData))
         self.assertEqual([[7, 8, 9]], self.otherListArrayContainer.list)
         self.assertTrue(np.array_equal(np.array([[10, 11, 12]]), self.otherListArrayContainer.array))
 
-    def test_whenExtendingAnEmptyArray_secondArrayShouldNotHaveReference(self):
+    def testWhenExtendingAnEmptyArray_secondArrayShouldNotHaveReference(self):
         self.otherListArrayContainer.append(np.array([[4, 5, 6]]))
         self.listArrayContainer.extend(self.otherListArrayContainer)
         thirdListArrayContainer = ListArrayContainer()
         thirdListArrayContainer.append(np.array([[7, 8, 9]]))
+
         self.listArrayContainer.extend(thirdListArrayContainer)
 
         self.assertTrue(np.array_equal(self.otherListArrayContainer.array, np.array([[4, 5, 6]])))
