59890d4e4f8f7d58a12516e97a6849257d00324a
Marc-André Vigneault <marc-andre.vigneault.02@hotmail.com>
2022-05-14 15:20:34 -0400

    add tqdm loading bars for parsing and converting 3d files

--- pytissueoptics/scene/loader/loader.py
+++ pytissueoptics/scene/loader/loader.py
@@ -4,7 +4,8 @@ from typing import List
 from pytissueoptics.scene.loader.parsers import OBJParser
 from pytissueoptics.scene.loader.parsers.parsedSurface import ParsedSurface
 from pytissueoptics.scene.solids import Solid
-from pytissueoptics.scene.geometry import Vector, Triangle, SurfaceCollection, Quad, Polygon, Vertex, primitives
+from pytissueoptics.scene.geometry import Vector, Triangle, SurfaceCollection, Vertex, primitives
+from pytissueoptics.scene.utils.mytqdm import tqdm
 
 
 class Loader:
@@ -46,6 +47,12 @@ class Loader:
         for vertex in self._parser.vertices:
             vertices.append(Vertex(*vertex))
 
+        totalProgressBarLength = 0
+        for objectName, _object in self._parser.objects.items():
+            totalProgressBarLength += len(_object.surfaces.items())
+        pbar = tqdm(total=totalProgressBarLength, desc="Converting File '{}'".format(self._filepath.split('/')[-1]),
+                    unit="surfaces")
+
         solids = []
         for objectName, _object in self._parser.objects.items():
             surfaces = SurfaceCollection()
@@ -53,7 +60,8 @@ class Loader:
                 surfaces.add(surfaceLabel, self._convertSurfaceToTriangles(surface, vertices))
             solids.append(Solid(position=Vector(0, 0, 0), vertices=vertices, surfaces=surfaces,
                                 primitive=primitives.POLYGON, label=objectName))
-
+            pbar.update(1)
+        pbar.close()
         return solids
 
     @staticmethod
