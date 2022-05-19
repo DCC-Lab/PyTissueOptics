4c445d18276b52f55086610b783b6d68afcb3238
JLBegin <ludovick.begin@gmail.com>
2022-05-19 15:15:34 -0400

    Test DirectionalSource

--- pytissueoptics/rayscattering/tests/testSource.py
+++ pytissueoptics/rayscattering/tests/testSource.py
@@ -5,7 +5,7 @@ from mockito import mock, when, verify
 
 from pytissueoptics.rayscattering import PencilPointSource, Photon
 from pytissueoptics.rayscattering.materials import ScatteringMaterial
-from pytissueoptics.rayscattering.source import Source, IsotropicPointSource
+from pytissueoptics.rayscattering.source import Source, IsotropicPointSource, DirectionalSource
 from pytissueoptics.rayscattering.tissues.rayScatteringScene import RayScatteringScene
 from pytissueoptics.scene import Logger
 from pytissueoptics.scene.geometry import Environment, Vector
@@ -53,6 +53,15 @@ class TestSource(unittest.TestCase):
         return photon
 
 
+class SinglePhotonSource(Source):
+    def __init__(self, position, photons):
+        super().__init__(position, N=len(photons), useHardwareAcceleration=False)
+        self._photons = photons
+
+    def getInitialPositionsAndDirections(self):
+        return np.array([[0, 0, 0]]), np.array([[0, 0, 1]])
+
+
 class TestPencilSource(unittest.TestCase):
     def testShouldHavePhotonsAllPointingInTheSourceDirection(self):
         sourceDirection = Vector(1, 0, 0)
@@ -75,10 +84,25 @@ class TestIsotropicPointSource(unittest.TestCase):
             self.assertEqual(sourcePosition, photon.position)
 
 
-class SinglePhotonSource(Source):
-    def __init__(self, position, photons):
-        super().__init__(position, N=len(photons), useHardwareAcceleration=False)
-        self._photons = photons
+class TestDirectionalSource(unittest.TestCase):
+    def testShouldHavePhotonsAllPointingInTheSourceDirection(self):
+        sourceDirection = Vector(1, 0, 0)
+        directionalSource = DirectionalSource(position=Vector(), direction=sourceDirection, diameter=1, N=10)
+        for photon in directionalSource.photons:
+            self.assertEqual(sourceDirection, photon.direction)
 
-    def getInitialPositionsAndDirections(self):
-        return np.array([[0, 0, 0]]), np.array([[0, 0, 1]])
+    def testShouldHavePhotonsUniformlyPositionedInsideTheSourceDiameter(self):
+        np.random.seed(0)
+        sourcePosition = Vector(3, 3, 0)
+        sourceDiameter = 2
+        directionalSourceTowardsY = DirectionalSource(position=sourcePosition, direction=Vector(0, 1, 0),
+                                                      diameter=sourceDiameter, N=10)
+        for photon in directionalSourceTowardsY.photons:
+            self.assertTrue(np.isclose(photon.position.y, sourcePosition.y))
+            self.assertTrue(photon.position.x <= sourcePosition.x + sourceDiameter / 2)
+            self.assertTrue(photon.position.x >= sourcePosition.x - sourceDiameter / 2)
+            self.assertTrue(photon.position.z <= sourcePosition.z + sourceDiameter / 2)
+            self.assertTrue(photon.position.z >= sourcePosition.z - sourceDiameter / 2)
+
+        self.assertFalse(all(photon.position.x == sourcePosition.x for photon in directionalSourceTowardsY.photons))
+        self.assertFalse(all(photon.position.z == sourcePosition.z for photon in directionalSourceTowardsY.photons))
