3b23f6c6d359d6d458cd42d4fb638da82f7a1317
JLBegin <ludovick.begin@gmail.com>
2022-10-19 19:51:44 -0400

    Fresnel:  reflection coefficient

--- pytissueoptics/rayscattering/opencl/src/fresnel.c
+++ pytissueoptics/rayscattering/opencl/src/fresnel.c
@@ -9,7 +9,45 @@ struct FresnelIntersection {
 typedef struct FresnelIntersection FresnelIntersection;
 
 
-void _createFresnelIntersection(FresnelIntersection* fresnelIntersection, float thetaIn) {
+float _getReflectionCoefficient(float n1, float n2, float thetaIn) {
+    if (n1 == n2) {
+        return 0;
+    }
+    if (thetaIn == 0) {
+        float R = (n2 - n1) / (n2 - n1);
+        return R * R;
+    }
+
+    float sa1 = sin(thetaIn);
+
+    float sa2 = sa1 * n1 / n2;
+    if (sa2 > 1) {
+        return 1;
+    }
+
+    float ca1 = sqrt(1 - sa1 * sa1);
+    float ca2 = sqrt(1 - sa2 * sa2);
+
+    float cap = ca1 * ca2 - sa1 * sa2;
+    float cam = ca1 * ca2 + sa1 * sa2;
+    float sap = sa1 * ca2 + ca1 * sa2;
+    float sam = sa1 * ca2 - ca1 * sa2;
+
+    return 0.5 * sam * sam * (cap * cap + cam * cam) / (sap * sap * cam * cam);
+}
+
+bool _getIsReflected(float nIn, float nOut, float thetaIn) {
+    float R = _getReflectionCoefficient(nIn, nOut, thetaIn);
+    if (R > 0.5) {  // fixme: we need to get a random number over here
+        return true;
+    }
+    return false;
+}
+
+void _createFresnelIntersection(FresnelIntersection* fresnelIntersection,
+                                float nIn, float nOut, float thetaIn) {
+    bool isReflected = _getIsReflected(nIn, nOut, thetaIn);
+
     // todo: implement refraction
     fresnelIntersection->isReflected = true;
     fresnelIntersection->angleDeflection = 2 * thetaIn - M_PI_F;
@@ -44,7 +82,7 @@ FresnelIntersection computeFresnelIntersection(float3 rayDirection, Intersection
 
     float thetaIn = acos(dot(normal, rayDirection));
 
-    _createFresnelIntersection(&fresnelIntersection, thetaIn);
+    _createFresnelIntersection(&fresnelIntersection, nIn, nOut, thetaIn);
 
     return fresnelIntersection;
 }
