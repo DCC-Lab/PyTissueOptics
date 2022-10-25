a56616c027e26a0851426216f7c95c633b30f5ed
JLBegin <ludovick.begin@gmail.com>
2022-10-25 11:55:55 -0400

    Update exampleComplex.py

--- pytissueoptics/rayscattering/opencl/exampleComplex.py
+++ pytissueoptics/rayscattering/opencl/exampleComplex.py
@@ -1,7 +1,7 @@
 from pytissueoptics import *
 
-material1 = ScatteringMaterial(mu_s=1.5, mu_a=1, g=0.8, n=1.4)
-material2 = ScatteringMaterial(mu_s=2.5, mu_a=1, g=0.8, n=1.7)
+material1 = ScatteringMaterial(mu_s=1.5, mu_a=1, g=0.9, n=1.4)
+material2 = ScatteringMaterial(mu_s=2.5, mu_a=1, g=0.9, n=1.7)
 
 cube = Cuboid(a=3, b=3, c=3, position=Vector(0, 0, 1.5), material=material1, label="Cube")
 
@@ -12,12 +12,12 @@ layerTissueScene = RayScatteringScene([cube, sphere])
 
 N = 500000
 logger = Logger()
-source = PencilPointSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1), N=N,
-                           useHardwareAcceleration=True)
+source = DirectionalSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1), N=N,
+                           useHardwareAcceleration=True, diameter=0.5)
 source.propagate(layerTissueScene, logger)
 
 stats = Stats(logger, source, layerTissueScene)
 stats.report()
 
-# stats.showEnergy3D(config=DisplayConfig(showPointsAsSpheres=False))
+stats.showEnergy3D(config=DisplayConfig(showPointsAsSpheres=False))
 
