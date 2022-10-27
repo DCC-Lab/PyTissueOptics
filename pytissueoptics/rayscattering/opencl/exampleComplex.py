c0497e946592283193dcabed7ee29a1c6dbd3418
JLBegin <ludovick.begin@gmail.com>
2022-10-27 12:00:19 -0400

    update examples

--- pytissueoptics/rayscattering/opencl/exampleComplex.py
+++ pytissueoptics/rayscattering/opencl/exampleComplex.py
@@ -1,7 +1,15 @@
 from pytissueoptics import *
 
-material1 = ScatteringMaterial(mu_s=1.5, mu_a=1, g=0.9, n=1.4)
-material2 = ScatteringMaterial(mu_s=2.5, mu_a=1, g=0.9, n=1.7)
+HIGH_SCATTERING = True
+
+if HIGH_SCATTERING:
+    N = 10000
+    material1 = ScatteringMaterial(mu_s=20, mu_a=0.1, g=0.9, n=1.4)
+    material2 = ScatteringMaterial(mu_s=30, mu_a=0.2, g=0.9, n=1.7)
+else:
+    N = 500000
+    material1 = ScatteringMaterial(mu_s=1.5, mu_a=1, g=0.9, n=1.4)
+    material2 = ScatteringMaterial(mu_s=2.5, mu_a=1, g=0.9, n=1.7)
 
 cube = Cuboid(a=3, b=3, c=3, position=Vector(0, 0, 1.5), material=material1, label="Cube")
 
@@ -10,7 +18,6 @@ sphere = Sphere(radius=1, order=3, position=Vector(0, 0, 1.5), material=material
 
 layerTissueScene = RayScatteringScene([cube, sphere])
 
-N = 500000
 logger = Logger()
 source = DirectionalSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1), N=N,
                            useHardwareAcceleration=True, diameter=0.5)
@@ -18,6 +25,5 @@ source.propagate(layerTissueScene, logger)
 
 stats = Stats(logger, source, layerTissueScene)
 stats.report()
-
 stats.showEnergy3D(config=DisplayConfig(showPointsAsSpheres=False))
 
--- pytissueoptics/rayscattering/opencl/exampleSimple.py
+++ pytissueoptics/rayscattering/opencl/exampleSimple.py
@@ -17,5 +17,8 @@ stats.report()
 
 # displayConfig = DisplayConfig(showPointsAsSpheres=False)
 # stats.showEnergy3D(config=displayConfig)
-stats.showEnergy2D(bins=101, logScale=True, limits=[[-1.5, 1.5], [0, 2]])
+limits = [[-1.5, 1.5], [0, 2]]
+stats.showEnergy2D(bins=101, logScale=True, limits=limits)
+stats.showEnergy2D(bins=101, solidLabel="middleLayer", logScale=True, limits=limits)
+stats.showEnergy2D(bins=101, solidLabel="middleLayer", surfaceLabel="interface0", logScale=True, limits=limits)
 
