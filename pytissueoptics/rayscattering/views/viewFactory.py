149ece8d39ee5fabe59a9f33327e9aa977f00659
JLBegin <ludovick.begin@gmail.com>
2023-01-11 18:55:09 -0500

    ViewFactory: implement default solid-specific views

--- pytissueoptics/rayscattering/views/viewFactory.py
+++ pytissueoptics/rayscattering/views/viewFactory.py
@@ -2,13 +2,11 @@ from typing import Union, List, Tuple
 
 from pytissueoptics.rayscattering.tissues import RayScatteringScene
 from pytissueoptics.rayscattering.views import ViewGroup, View2D, Direction
-from pytissueoptics.scene.solids import Solid
 
 
 class ViewFactory:
     def __init__(self, scene: RayScatteringScene, defaultBinSize: Union[float, Tuple[float, float, float]]):
         self._scene = scene
-        self._sceneLimits = scene.getBoundingBox().xyzLimits
 
         self._defaultBinSize3D = defaultBinSize
         if isinstance(self._defaultBinSize3D, float):
@@ -28,27 +26,34 @@ class ViewFactory:
     def _createFromGroup(self, viewGroup: ViewGroup):
         # todo: correctly implement all ViewGroups with bitflag logic.
         views = []
-        if viewGroup == ViewGroup.SCENE:
+        if ViewGroup.SCENE in viewGroup:
             views += self._getDefaultViewsXYZ()
-        elif viewGroup == ViewGroup.SOLIDS:
+
+        if ViewGroup.SOLIDS in viewGroup:
             for solid in self._scene.solids:
-                views += self._getDefaultSolidViewsXYZ(solid)
-        else:
-            raise NotImplementedError("Default views for ViewGroup {} not implemented.".format(viewGroup))
+                views += self._getDefaultViewsXYZ(solidLabel=solid.getLabel())
+
+        if ViewGroup.SURFACES_ENTERING in viewGroup:
+            raise NotImplementedError("Default views for ViewGroup.SURFACES not implemented.".format(viewGroup))
+
+        if ViewGroup.SURFACES_LEAVING in viewGroup:
+            raise NotImplementedError("Default views for ViewGroup.SURFACES not implemented.".format(viewGroup))
 
         return views
 
     @staticmethod
-    def _getDefaultViewsXYZ() -> List[View2D]:
-        return [View2D(Direction.X_POS, Direction.Z_POS),
-                View2D(Direction.Y_NEG, Direction.Z_POS),
-                View2D(Direction.Z_POS, Direction.X_NEG)]
-
-    def _getDefaultSolidViewsXYZ(self, solid: Solid) -> List[View2D]:
-        raise NotImplementedError()
+    def _getDefaultViewsXYZ(solidLabel: str = None) -> List[View2D]:
+        return [View2D(Direction.X_POS, Direction.Z_POS, solidLabel=solidLabel),
+                View2D(Direction.Y_NEG, Direction.Z_POS, solidLabel=solidLabel),
+                View2D(Direction.Z_POS, Direction.X_NEG, solidLabel=solidLabel)]
 
     def _setContext(self, view: View2D):
-        limits3D = [(d[0], d[1]) for d in self._sceneLimits]
+        if view.solidLabel:
+            solid = self._scene.getSolid(view.solidLabel)
+            limits3D = solid.getBoundingBox().xyzLimits
+        else:
+            limits3D = self._scene.getBoundingBox().xyzLimits
+        limits3D = [(d[0], d[1]) for d in limits3D]
 
         limits = (limits3D[view.axisU], limits3D[view.axisV])
         binSize = (self._defaultBinSize3D[view.axisU], self._defaultBinSize3D[view.axisV])
