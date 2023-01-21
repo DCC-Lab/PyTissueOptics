d08395f4f49daa0a31a957faff55f2e15c9852e5
JLBegin <ludovick.begin@gmail.com>
2023-01-21 15:03:00 -0500

    refactor ListArrayContainer

--- pytissueoptics/scene/logger/listArrayContainer.py
+++ pytissueoptics/scene/logger/listArrayContainer.py
@@ -1,4 +1,5 @@
 import copy
+from typing import Optional
 
 import numpy as np
 
@@ -25,7 +26,16 @@ class ListArrayContainer:
         else:
             return None
 
+    def _assertSameWidth(self, data):
+        if self._width is None:
+            return
+        if isinstance(data, list):
+            assert len(data) == self._width
+        elif isinstance(data, np.ndarray):
+            assert data.shape[1] == self._width
+
     def append(self, item):
+        self._assertSameWidth(item)
         if isinstance(item, list):
             if self._list is None:
                 self._list = [copy.deepcopy(item)]
@@ -42,23 +52,15 @@ class ListArrayContainer:
             self._list = copy.deepcopy(other._list)
         elif other._list is not None:
             self._list.extend(other._list)
+        if other._array is not None:
+            self.append(other._array)
 
+    def getData(self) -> Optional[np.ndarray]:
+        if self._list is None and self._array is None:
+            return None
+        if self._list is None:
+            return self._array
         if self._array is None:
-            self._array = copy.deepcopy(other._array)
-        elif other._array is not None:
-            self._array = np.concatenate((self._array, other._array), axis=0)
-
-    def getData(self):
-        if self._list is not None and self._array is not None:
-            listArray = np.array(self._list)
-            if listArray.shape[1] == self._array.shape[1]:
-                mergedData = np.concatenate((listArray, self._array), axis=0)
-            else:
-                raise ValueError("Cannot merge list data and array data with mismatched column count.")
-        elif self._list is None:
-            mergedData = self._array
-        elif self._array is None:
-            mergedData = np.array(self._list)
-        else:
-            mergedData = None
+            return np.array(self._list)
+        mergedData = np.concatenate((np.array(self._list), self._array), axis=0)
         return mergedData
