6ab859e10ce3b1f7caad580894dbc9643a8e9c78
JLBegin <ludovick.begin@gmail.com>
2023-01-22 17:28:14 -0500

    finish testing scene kit : 99% coverage

---
+++ pytissueoptics/scene/tests/utils/testNoProgressBar.py
@@ -0,0 +1,13 @@
+import unittest
+
+from pytissueoptics.scene.utils.progressBar import noProgressBar
+
+
+class TestNoProgressBar(unittest.TestCase):
+    def testWhenUsingNoProgressBarWithTQDMArguments_shouldWarnAndIterate(self):
+        value = 0
+        with self.assertWarns(UserWarning):
+            pbar = noProgressBar([1, 2, 3], desc="A description")
+        for element in pbar:
+            value += element
+        self.assertEqual(6, value)
