6c93e6467f01993b91eb7d469595078917a26966
JLBegin <ludovick.begin@gmail.com>
2023-01-22 11:37:31 -0500

    improve test coverage of tree splitConstructor

--- pytissueoptics/scene/tests/tree/treeConstructor/binary/testSplitConstructor.py
+++ pytissueoptics/scene/tests/tree/treeConstructor/binary/testSplitConstructor.py
@@ -29,11 +29,18 @@ class TestSplitConstructor(unittest.TestCase):
         splitAxis = "x"
         self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
         normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
-        left, right = self._fbtc._splitTriangles(normal, dot)
-        expectedLeft = [Triangle(Vertex(0, 0, 0), Vertex(0.5, 0.5, 0.5), Vertex(0.5, -0.5, 0.5))]
 
-        self.assertEqual(left[0], expectedLeft[0])
-        self.assertEqual(len(right), 2)
+        left, right = self._fbtc._splitTriangles(normal, dot)
+
+        expectedLeft = Triangle(Vertex(0, 0, 0), Vertex(0.5, 0.5, 0.5), Vertex(0.5, -0.5, 0.5))
+        expectedRight1 = Triangle(Vertex(0.5, 0.5, 0.5), Vertex(1, 1, 1), Vertex(0.5, -0.5, 0.5))
+        expectedRight2 = Triangle(Vertex(0.5, -0.5, 0.5), Vertex(1, 1, 1), Vertex(1, -1, 1))
+
+        self.assertEqual(1, len(left))
+        self.assertEqual(2, len(right))
+        self.assertIn(expectedLeft, left)
+        self.assertIn(expectedRight1, right)
+        self.assertIn(expectedRight2, right)
 
     def testGivenAPolygonAndAPlane_whenSplittingPolygon_splitPolygonShouldHaveGoodEnvironmentAndNormal(self):
         myEnvironment = Environment("A material")
@@ -42,7 +49,9 @@ class TestSplitConstructor(unittest.TestCase):
         splitAxis = "y"
         self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
         normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
         left, right = self._fbtc._splitTriangles(normal, dot)
+
         expectedLeft = [Triangle(Vertex(0, 0, 0), Vertex(0, 0.5, 0), Vertex(0, 0.5, 0.5), insideEnvironment=myEnvironment)]
 
         self.assertEqual(left[0], expectedLeft[0])
@@ -50,7 +59,6 @@ class TestSplitConstructor(unittest.TestCase):
         self.assertEqual(Vector(1, 0, 0), left[0].normal)
         self.assertEqual(Vector(1, 0, 0), right[0].normal)
         self.assertEqual(Vector(1, 0, 0), right[1].normal)
-
         self.assertEqual(len(right), 2)
 
     def testGivenAPolygonAndAPlane_whenSplittingOnAVertexInside_shouldReturn2Polygons(self):
@@ -59,23 +67,87 @@ class TestSplitConstructor(unittest.TestCase):
         splitAxis = "y"
         self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
         normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
         left, right = self._fbtc._splitTriangles(normal, dot)
+
         expectedLeft = [Triangle(Vertex(1, -1, 1), Vertex(1, 0, 1), Vertex(0, 0, 0))]
         expectedRight = [Triangle(Vertex(1, 1, 1), Vertex(1, 0, 1), Vertex(0, 0, 0))]
+
         self.assertEqual(left[0], expectedLeft[0])
         self.assertEqual(right[0], expectedRight[0])
 
-    def testGivenAPolygonAndAPlane_whenSplittingOnAVertexOutside_shouldReturn1Polygons(self):
+    def testGivenAPolygonAndAPlane_whenBarelySplittingOnAVertexInside_shouldReturn2Polygons(self):
+        tolerance = 0.000001
+        toBeSplit = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 1), Vertex(1, -tolerance, 1))]
+        splitValue = 0
+        splitAxis = "y"
+        self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
+        normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
+        left, right = self._fbtc._splitTriangles(normal, dot)
+
+        expectedLeft = [Triangle(Vertex(1, -tolerance, 1), Vertex(1, 0, 1), Vertex(0, 0, 0))]
+        expectedRight = [Triangle(Vertex(1, 1, 1), Vertex(1, 0, 1), Vertex(0, 0, 0))]
+
+        self.assertEqual(left[0], expectedLeft[0])
+        self.assertEqual(right[0], expectedRight[0])
+
+    def testGivenAPolygonAndAPlane_whenBarelySplittingBetweenTwoVertices_shouldReturn3Polygons(self):
+        tolerance = 0.000001
+        toBeSplit = [Triangle(Vertex(0, tolerance, 0), Vertex(1, 1, 0), Vertex(1, -tolerance, 0))]
+        splitValue = 0
+        splitAxis = "y"
+        self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
+        normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
+        left, right = self._fbtc._splitTriangles(normal, dot)
+
+        self.assertEqual(1, len(left))
+        self.assertEqual(2, len(right))
+
+    def testGivenAPolygonAndAPlane_whenBarelySplittingOnTwoVertices_shouldNotSplit(self):
+        tolerance = 0.000001/2
+        toBeSplit = [Triangle(Vertex(0, tolerance, 0), Vertex(1, 1, 0), Vertex(1, -tolerance, 0))]
+        splitValue = 0
+        splitAxis = "y"
+        self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
+        normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
+        left, right = self._fbtc._splitTriangles(normal, dot)
+
+        self.assertEqual(0, len(left))
+        self.assertEqual(1, len(right))
+        self.assertEqual(toBeSplit[0], right[0])
+
+    def testGivenAPolygonAndAPlane_whenSplittingOnAVertexOutsideRight_shouldReturn1PolygonLeft(self):
         toBeSplit = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 1), Vertex(1, -1, 1))]
         splitValue = 1
         splitAxis = "y"
         self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
         normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
         left, right = self._fbtc._splitTriangles(normal, dot)
+
         expectedLeft = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 1), Vertex(1, -1, 1))]
         expectedRight = []
+
         self.assertEqual(left[0], expectedLeft[0])
         self.assertEqual(right, expectedRight)
+
+    def testGivenAPolygonAndAPlane_whenSplittingOnAVertexOutsideLeft_shouldReturn1PolygonRight(self):
+        toBeSplit = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 1), Vertex(1, -1, 1))]
+        splitValue = -1
+        splitAxis = "y"
+        self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
+        normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
+        left, right = self._fbtc._splitTriangles(normal, dot)
+
+        expectedRight = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 1), Vertex(1, -1, 1))]
+        expectedLeft = []
+
+        self.assertEqual(left, expectedLeft)
+        self.assertEqual(right[0], expectedRight[0])
 
     def testGivenAPolygonAndAPlane_whenSplittingOn2VerticesOutside_shouldReturn1Polygons(self):
         toBeSplit = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 1), Vertex(1, -1, 1))]
@@ -83,11 +155,28 @@ class TestSplitConstructor(unittest.TestCase):
         splitAxis = "x"
         self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
         normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
         left, right = self._fbtc._splitTriangles(normal, dot)
+
         expectedLeft = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 1), Vertex(1, -1, 1))]
         expectedRight = []
+
         self.assertEqual(left[0], expectedLeft[0])
         self.assertEqual(right, expectedRight)
+
+    def testGivenAPolygonAndAPlane_whenSplittingOnPolygonPlane_shouldNotSplitAndReturnInBothLeftAndRight(self):
+        toBeSplit = [Triangle(Vertex(0, 0, 0), Vertex(1, 1, 0), Vertex(1, -1, 0))]
+        splitValue = 0
+        splitAxis = "z"
+        self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
+        normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
+        left, right = self._fbtc._splitTriangles(normal, dot)
+
+        self.assertEqual(1, len(left))
+        self.assertEqual(1, len(right))
+        self.assertEqual(left[0], toBeSplit[0])
+        self.assertEqual(right[0], toBeSplit[0])
 
     def testGivenUltraThinPolygon_whenSplitting_shouldStillReturn2Polygons(self):
         vertices = [Vertex(8.860660171779822, 5.000000000000001, -4.9455),
@@ -98,7 +187,9 @@ class TestSplitConstructor(unittest.TestCase):
         splitValue = 4.999868997359811
         self._fbtc.result = SAHSearchResult([], [], toBeSplit, None, None, splitAxis, splitValue)
         normal, dot = self._fbtc._makeSplitPlane(splitAxis, splitValue)
+
         left, right = self._fbtc._splitTriangles(normal, dot)
+
         self.assertEqual(1, len(left))
         self.assertEqual(1, len(right))
 
@@ -115,8 +206,10 @@ class TestSplitConstructor(unittest.TestCase):
         nodeBbox = BoundingBox([-10, 10], [-10, 10], [-10, 10])
         node = Node(polygons=polygons, bbox=nodeBbox)
         fbtc = SplitThreeAxesConstructor(traversalCost=8, intersectionCost=2)
+
         splitNodeResult = fbtc._splitNode(node)
         groups = splitNodeResult.polygonGroups
+
         self.assertEqual(2, len(groups))
         self.assertListEqual(expectedLeft, groups[0])
         self.assertListEqual(expectedRight, groups[1])
