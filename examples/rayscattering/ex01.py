84bdf6051dd8de63990f2a85622f8f4dce31cad5
JLBegin <ludovick.begin@gmail.com>
2023-09-13 23:01:01 -0400

    combine a few old examples

--- examples/rayscattering/ex01.py
+++ examples/rayscattering/ex01.py
@@ -1,8 +1,8 @@
 from pytissueoptics import *
 
-TITLE = "PencilSource propagation through a multi-layered tissue"
+TITLE = "Divergent source propagation through a multi-layered tissue"
 
-DESCRIPTION = """ Propagation of a pencil source through a sample tissue called PhantomTissue. This tissue is composed 
+DESCRIPTION = """ Propagation of a divergent source through a sample tissue called PhantomTissue. This tissue is composed 
 of a stacked cuboid made of 3 layers of different material. """
 
 
@@ -11,19 +11,20 @@ def exampleCode():
 
     tissue = samples.PhantomTissue()
     logger = EnergyLogger(tissue)
-    source = PencilPointSource(position=Vector(0, 0, -1), direction=Vector(0, 0, 1), N=N)
+    source = DivergentSource(position=Vector(0, 0, -0.1), direction=Vector(0, 0, 1), N=N,
+                             diameter=0.1, divergence=0.4)
 
     source.propagate(tissue, logger=logger)
 
     viewer = Viewer(tissue, source, logger)
     viewer.reportStats()
 
-    viewer.show3D()
-    viewer.show3D(pointCloudStyle=PointCloudStyle(showSolidPoints=False))
     viewer.show2D(View2DProjectionX())
     viewer.show2D(View2DProjectionX(solidLabel="middleLayer"))
     viewer.show2D(View2DSurfaceZ(solidLabel="middleLayer", surfaceLabel="interface1", surfaceEnergyLeaving=False))
     viewer.show1D(Direction.Z_POS)
+    viewer.show3D()
+    viewer.show3D(pointCloudStyle=PointCloudStyle(showSolidPoints=False))
 
 
 if __name__ == "__main__":
