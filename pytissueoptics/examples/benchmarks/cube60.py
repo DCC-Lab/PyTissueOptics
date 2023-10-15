4bc8e69364a18efb6c6ca45ced399eeeb4743172
jlbegin <ludovick.begin@gmail.com>
2023-10-15 17:18:31 -0400

    Update cube60.py

--- pytissueoptics/examples/benchmarks/cube60.py
+++ pytissueoptics/examples/benchmarks/cube60.py
@@ -3,25 +3,15 @@ from pytissueoptics import *
 
 TITLE = "MCX Homogeneous cube"
 
-DESCRIPTION = """ Pencil source propagation through a homogeneous cube of size 60x60x60. """
-
-
-"""
-    np.random.seed(9265358)
-    N = 10000 
-    
-    hanged
-"""
+DESCRIPTION = """ Pencil source propagation through a homogeneous cube of size 60x60x60 mm. """
 
 
 def exampleCode():
-    import numpy as np
-    np.random.seed(9265358)
     N = 100000 if hardwareAccelerationIsAvailable() else 1000
 
     tissue = ScatteringScene([Cube(60, material=ScatteringMaterial(mu_a=0.005, mu_s=1, g=0.01, n=1))])
-    logger = EnergyLogger(tissue, keep3D=False, defaultBinSize=0.1)
-    source = PencilPointSource(position=Vector(0, 0, 10), direction=Vector(0, 0, 1), N=N)
+    logger = EnergyLogger(tissue, defaultBinSize=0.1)
+    source = PencilPointSource(position=Vector(0, 0, -29.99), direction=Vector(0, 0, 1), N=N, displaySize=1)
 
     source.propagate(tissue, logger=logger)
 
@@ -30,8 +20,8 @@ def exampleCode():
 
     viewer.show2D(View2DProjectionX())
     viewer.show1D(Direction.Z_POS)
-    # viewer.show3D()
-    # viewer.show3D(pointCloudStyle=PointCloudStyle(showSolidPoints=False))
+    viewer.show3D(pointCloudStyle=PointCloudStyle(showSolidPoints=False))
+    viewer.show3D()
 
 
 if __name__ == "__main__":
