200447204f2e603b5431d4319c19828b175505ea
Marc-André Vigneault <31829033+PyMarc2@users.noreply.github.com>
2024-05-02 22:11:03 -0400

    update different scenes for validation + bash script for parameter sweep

--- pytissueoptics/examples/benchmarks/cube60.py
+++ pytissueoptics/examples/benchmarks/cube60.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import env
 from pytissueoptics import *
 
@@ -8,20 +10,24 @@ DESCRIPTION = """ Pencil source propagation through a homogeneous cube of size 6
 
 def exampleCode():
     N = 100000 if hardwareAccelerationIsAvailable() else 1000
+    absorbancess = []
+    for ua in np.linspace(0.001, 0.05, 50):
+        tissue = ScatteringScene([Cube(60, material=ScatteringMaterial(mu_a=ua, mu_s=1, g=0.01, n=1), label="cube")])
+        logger = EnergyLogger(tissue, defaultBinSize=0.1)
+        source = PencilPointSource(position=Vector(0, 0, -29.99), direction=Vector(0, 0, 1), N=N, displaySize=1)
 
-    tissue = ScatteringScene([Cube(60, material=ScatteringMaterial(mu_a=0.005, mu_s=1, g=0.01, n=1))])
-    logger = EnergyLogger(tissue, defaultBinSize=0.1)
-    source = PencilPointSource(position=Vector(0, 0, -29.99), direction=Vector(0, 0, 1), N=N, displaySize=1)
+        source.propagate(tissue, logger=logger)
 
-    source.propagate(tissue, logger=logger)
+        stats = Stats(logger)
 
-    viewer = Viewer(tissue, source, logger)
-    viewer.reportStats()
+        absorbancess.append((ua, stats.getAbsorbance("cube")))
+        #viewer.reportStats()
 
-    viewer.show2D(View2DProjectionX())
-    viewer.show1D(Direction.Z_POS)
-    viewer.show3D(pointCloudStyle=PointCloudStyle(showSolidPoints=False))
-    viewer.show3D()
+        #viewer.show2D(View2DProjectionX())
+        #viewer.show1D(Direction.Z_POS)
+        #viewer.show3D(pointCloudStyle=PointCloudStyle(showSolidPoints=False))
+        #viewer.show3D()
+    print(*absorbancess, sep="\n")
 
 
 if __name__ == "__main__":
